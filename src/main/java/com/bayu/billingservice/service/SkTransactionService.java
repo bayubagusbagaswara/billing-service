01a5c4e6f7ed0a22d5631d5793e24fbd51f0614f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-04 15:43:46 +0700

    add some method contract

--- src/main/java/com/bayu/billingservice/service/SkTransactionService.java
+++ src/main/java/com/bayu/billingservice/service/SkTransactionService.java
@@ -1,4 +1,15 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.model.SkTransaction;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface SkTransactionService {
+
+    List<SkTransaction> readFileAndInsertToDB(String filePath) throws IOException, CsvException;
+    List<SkTransaction> getAllByPortfolioCode(String portfolioCode);
+    List<SkTransaction> getAllByPortfolioCodeAndSystem(String portfolioCode, String system);
+
 }
