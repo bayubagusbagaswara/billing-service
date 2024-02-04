b72aefbbf5f6677014be55f60c3925901f798a02
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-04 14:31:02 +0700

    add model SfValRgDaily

---
+++ src/main/java/com/bayu/billingservice/model/SfValRgDaily.java
@@ -0,0 +1,47 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "sf_val_rg_daily")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SfValRgDaily {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "batch")
+    private Integer batch;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "aid")
+    private String aid; // or portfolio code
+
+    @Column(name = "security_name")
+    private String securityName;
+
+    @Column(name = "face_value")
+    private BigDecimal faceValue;
+
+    @Column(name = "market_price")
+    private String marketPrice;
+
+    @Column(name = "market_value")
+    private BigDecimal marketValue; // is faceValue * marketPrice
+
+    @Column(name = "estimation_sk_fee")
+    private BigDecimal estimationSafekeepingFee;
+}
