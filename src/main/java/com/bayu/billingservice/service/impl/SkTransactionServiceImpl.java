84b5fb507c8fe70e21f5926f3d84520a4dede84c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-04 15:54:43 +0700

    implement logic service sk transaction

--- src/main/java/com/bayu/billingservice/service/impl/SkTransactionServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/SkTransactionServiceImpl.java
@@ -1,14 +1,46 @@
 package com.bayu.billingservice.service.impl;
 
+import com.bayu.billingservice.model.SkTransaction;
 import com.bayu.billingservice.repository.SkTransactionRepository;
+import com.bayu.billingservice.service.SkTransactionService;
+import com.bayu.billingservice.util.CsvDataMapper;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class SkTransactionServiceImpl {
+public class SkTransactionServiceImpl implements SkTransactionService {
 
     private final SkTransactionRepository skTransactionRepository;
+
+    @Override
+    public List<SkTransaction> readFileAndInsertToDB(String filePath) throws IOException, CsvException {
+        log.info("Start read and insert SkTransaction to the database : {}", filePath);
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            List<String[]> rows = reader.readAll();
+
+            List<SkTransaction> skTransactionList = CsvDataMapper.mapCsvSkTransaction(rows);
+
+            return skTransactionRepository.saveAll(skTransactionList);
+        }
+    }
+
+    @Override
+    public List<SkTransaction> getAllByPortfolioCode(String portfolioCode) {
+        return skTransactionRepository.findAllByPortfolioCode(portfolioCode);
+    }
+
+    @Override
+    public List<SkTransaction> getAllByPortfolioCodeAndSystem(String portfolioCode, String system) {
+        return skTransactionRepository.findAllByPortfolioCodeAndSystem(portfolioCode, system);
+    }
+
 }
