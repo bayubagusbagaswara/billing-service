9e039abfa389b4a4fbb94d93c0f0e47811deb5d1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-04 15:51:12 +0700

    add some method repository

--- src/main/java/com/bayu/billingservice/repository/SkTransactionRepository.java
+++ src/main/java/com/bayu/billingservice/repository/SkTransactionRepository.java
@@ -4,7 +4,12 @@ import com.bayu.billingservice.model.SkTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SkTransactionRepository extends JpaRepository<SkTransaction, Long> {
+
+    List<SkTransaction> findAllByPortfolioCode(String portfolioCode);
+    List<SkTransaction> findAllByPortfolioCodeAndSystem(String portfolioCode, String system);
 
 }
