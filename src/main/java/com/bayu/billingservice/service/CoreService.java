0cb72c91b972624670614c025f9729cdf2ca55b2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-04 21:43:35 +0700

    add service contract CoreService

---
+++ src/main/java/com/bayu/billingservice/service/CoreService.java
@@ -0,0 +1,26 @@
+package com.bayu.billingservice.service;
+
+public interface CoreService {
+
+    // type 1
+
+    // type 2
+
+    // type 3
+
+    // type 4
+
+    // type 5
+
+    // type 6
+
+    // type 7
+
+    // type 8
+
+    // type 9
+
+    // type 10
+
+    // type 11
+}
