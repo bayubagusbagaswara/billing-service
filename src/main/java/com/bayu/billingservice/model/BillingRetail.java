91c3c1a1c1e745a992ee171f1db4ccf60f47d906
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-08 19:29:29 +0700

    add model BillingRetail

---
+++ src/main/java/com/bayu/billingservice/model/BillingRetail.java
@@ -0,0 +1,20 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "billing_retails")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingRetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
