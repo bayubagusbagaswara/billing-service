67c1b383043c2f5f9cc472c08a5d59f9ae8d9210
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-10 10:01:44 +0700

    add service contract KSEISafekeepingFeeService

---
+++ src/main/java/com/bayu/billingservice/service/KSEISafekeepingFeeService.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.KSEISafekeepingFee;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface KSEISafekeepingFeeService {
+
+    List<KSEISafekeepingFee> readAndInsert(String filePath) throws IOException, CsvException;
+
+    List<KSEISafekeepingFee> getAll();
+
+
+}
