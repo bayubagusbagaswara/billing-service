af4440829b9fbddbbd74cc252b328d66c89a7291
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-11 14:38:21 +0700

    add handler exception

--- src/main/java/com/bayu/billingservice/controller/GlobalExceptionHandler.java
+++ src/main/java/com/bayu/billingservice/controller/GlobalExceptionHandler.java
@@ -2,11 +2,14 @@ package com.bayu.billingservice.controller;
 
 import com.bayu.billingservice.dto.ResponseDTO;
 import com.bayu.billingservice.exception.CsvProcessingException;
+import com.bayu.billingservice.exception.ExcelProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +25,28 @@ public class GlobalExceptionHandler {
                 .payload(null)
                 .build();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(ExcelProcessingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ResponseDTO<String>> handleExcelProcessingException(ExcelProcessingException ex) {
+        ResponseDTO<String> response = ResponseDTO.<String>builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(ex.getMessage())
+                .payload(null)
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleIOException(IOException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading the Excel file: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to process Excel file: " + ex.getMessage());
     }
 }
