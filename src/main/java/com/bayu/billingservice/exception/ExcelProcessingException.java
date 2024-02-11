867a3623483204fd04b36b95eb867b2e4a3a91ca
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-11 14:37:56 +0700

    add exception ExcelProcessingException

---
+++ src/main/java/com/bayu/billingservice/exception/ExcelProcessingException.java
@@ -0,0 +1,9 @@
+package com.bayu.billingservice.exception;
+
+public class ExcelProcessingException extends RuntimeException {
+
+    public ExcelProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
