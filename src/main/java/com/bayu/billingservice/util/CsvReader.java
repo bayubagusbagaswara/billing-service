cb91d8ffd2fc9bf445eaea38bd48b18522527d1f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 17:19:24 +0700

    add util CsvReader

---
+++ src/main/java/com/bayu/billingservice/util/CsvReader.java
@@ -0,0 +1,22 @@
+package com.bayu.billingservice.util;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@UtilityClass
+public class CsvReader {
+
+    public static List<String[]> readCsvFile(String filePath) throws IOException, CsvException {
+        try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
+            return csvReader.readAll();
+        }
+    }
+
+}
