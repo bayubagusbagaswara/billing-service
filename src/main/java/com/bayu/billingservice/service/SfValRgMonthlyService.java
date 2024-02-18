5299f90c66909fe6ed11801cbb6ffb326e2003d3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 20:48:36 +0700

    add service contract SfValRgMonthlyService

---
+++ src/main/java/com/bayu/billingservice/service/SfValRgMonthlyService.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.SfValRgMonthly;
+
+import java.util.List;
+
+public interface SfValRgMonthlyService {
+
+    String readFileAndInsertToDB(String filePath);
+
+    List<SfValRgMonthly> getAll();
+
+    List<SfValRgMonthly> getAllByAid(String aid);
+
+    SfValRgMonthly getByAidAndSecurityName(String aid, String securityName);
+}
