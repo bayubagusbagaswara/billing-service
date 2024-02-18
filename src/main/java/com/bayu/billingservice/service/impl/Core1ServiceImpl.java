1a71a84ac6f64bd13d5f5eb801241970d5cc5120
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 15:45:42 +0700

    add service impl Core1ServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/Core1ServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.Core1Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class Core1ServiceImpl implements Core1Service {
+
+    List<Core1DTO>
+}
