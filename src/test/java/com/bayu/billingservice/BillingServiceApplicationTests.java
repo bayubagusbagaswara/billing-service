116ac5938d48058e816448711bf9031246b52aae
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 16:17:15 +0700

    add unit test

--- src/test/java/com/bayu/billingservice/BillingServiceApplicationTests.java
+++ src/test/java/com/bayu/billingservice/BillingServiceApplicationTests.java
@@ -3,6 +3,11 @@ package com.bayu.billingservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 @SpringBootTest
 class BillingServiceApplicationTests {
 
@@ -10,4 +15,16 @@ class BillingServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testDisplayMonthFullName() {
+		// Create a LocalDate object
+		LocalDate localDate = LocalDate.now();
+
+		// Get the month from the LocalDate object
+		Month month = localDate.getMonth();
+
+		// Get the full month name in the default locale
+		String fullMonthName = month.getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+		System.out.println("Full Month Name: " + fullMonthName);
+	}
 }
