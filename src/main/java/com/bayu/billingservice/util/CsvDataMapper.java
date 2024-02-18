261adaa8cda008d60913a3fbb283a7d9696d1fc1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 20:22:01 +0700

    update all method

--- src/main/java/com/bayu/billingservice/util/CsvDataMapper.java
+++ src/main/java/com/bayu/billingservice/util/CsvDataMapper.java
@@ -6,17 +6,18 @@ import com.bayu.billingservice.model.SkTransaction;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @UtilityClass
 @Slf4j
 public class CsvDataMapper {
 
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d-MMM-yy");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d-MMM-yy");
     private static final DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("d/M/yyyy");
 
     public static List<SkTransaction> mapCsvSkTransaction(List<String[]> rows) {
@@ -24,21 +25,27 @@ public class CsvDataMapper {
         log.info("[Start Map CSV] SKTran Rows : {}", rows.size());
 
         for (String[] row : rows) {
+            LocalDate date = ConvertDateUtil.parseDateOrDefault(row[7], dateTimeFormatter);
+            Integer year = date != null ? date.getYear() : null;
+            String monthName = date != null ? date.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) : null;
+
             SkTransaction skTransaction = SkTransaction.builder()
-                    .tradeId(processString(row[0]))
-                    .portfolioCode(processString(row[1]))
-                    .securityType(processString(row[2]))
-                    .securityShortName(processString(row[3]))
-                    .securityName(processString(row[4]))
-                    .type(processString(row[5]))
-                    .tradeDate(parseDateOrDefault(row[6], dateFormatter, null))
-                    .settlementDate(parseDateOrDefault(row[7], dateFormatter, null))
-                    .amount(parseBigDecimalOrDefault(row[8].replace(".", ""), null))
-                    .currency(processString(row[9]))
-                    .deleteStatus(processString(row[10]))
-                    .settlementSystem(processString(row[12]))
-                    .sid(processString(row[14]))
-                    .remark(removeWhitespaceBetweenCharacters(row[15]))
+                    .tradeId(StringUtil.processString(row[0]))
+                    .portfolioCode(StringUtil.processString(row[1]))
+                    .securityType(StringUtil.processString(row[2]))
+                    .securityShortName(StringUtil.processString(row[3]))
+                    .securityName(StringUtil.processString(row[4]))
+                    .type(StringUtil.processString(row[5]))
+                    .tradeDate(ConvertDateUtil.parseDateOrDefault(row[6], dateTimeFormatter))
+                    .settlementDate(date)
+                    .month(monthName)
+                    .year(year)
+                    .amount(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[8].replace(".", "")))
+                    .currency(StringUtil.processString(row[9]))
+                    .deleteStatus(StringUtil.processString(row[10]))
+                    .settlementSystem(StringUtil.processString(row[12]))
+                    .sid(StringUtil.processString(row[14]))
+                    .remark(StringUtil.removeWhitespaceBetweenCharacters(row[15]))
                     .build();
 
             skTransactionList.add(skTransaction);
@@ -51,17 +58,25 @@ public class CsvDataMapper {
     public static List<SfValRgDaily> mapCsvSfValRgDaily(List<String[]> rows) {
         List<SfValRgDaily> sfValRgDailyList = new ArrayList<>();
         log.info("[Start Map CSV] SfVal RG Daily Rows : {}", rows.size());
+
         for (String[] row : rows) {
+            LocalDate date = ConvertDateUtil.parseDateOrDefault(row[1], dateTimeFormatter1);
+            Integer year = date != null ? date.getYear() : null;
+            String monthName = date != null ? date.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) : null;
+
             SfValRgDaily sfValRgDaily = SfValRgDaily.builder()
-                    .batch(parseIntOrDefault(row[0]))
-                    .date(parseDateOrDefault(row[1], dateTimeFormatter1, null))
-                    .aid(processString(row[2]))
-                    .securityName(processString(row[3]))
-                    .faceValue(parseBigDecimalOrDefault(row[4], null))
+                    .batch(ConvertIntegerUtil.parseIntOrDefault(row[0]))
+                    .date(ConvertDateUtil.parseDateOrDefault(row[1], dateTimeFormatter1))
+                    .month(monthName)
+                    .year(year)
+                    .aid(StringUtil.processString(row[2]))
+                    .securityName(StringUtil.processString(row[3]))
+                    .faceValue(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[4]))
                     .marketPrice(row[5])
-                    .marketValue(parseBigDecimalOrDefault(row[6], null))
-                    .estimationSafekeepingFee(parseBigDecimalOrDefault(row[7], null))
+                    .marketValue(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[6]))
+                    .estimationSafekeepingFee(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[7]))
                     .build();
+
             sfValRgDailyList.add(sfValRgDaily);
         }
 
@@ -74,62 +89,28 @@ public class CsvDataMapper {
         log.info("[Start Map CSV] SfVal RG Monthly Rows : {}", rows.size());
 
         for (String[] row : rows) {
+            LocalDate date = ConvertDateUtil.parseDateOrDefault(row[1], dateTimeFormatter1);
+            Integer year = date != null ? date.getYear() : null;
+            String monthName = date != null ? date.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH) : null;
+
             SfValRgMonthly sfValRgMonthly = SfValRgMonthly.builder()
-                    .batch(parseIntOrDefault(row[0]))
-                    .date(parseDateOrDefault(row[1], dateTimeFormatter1, null))
-                    .aid(processString(row[2]))
-                    .securityName(processString(row[3]))
-                    .faceValue(parseBigDecimalOrDefault(row[4], null))
+                    .batch(ConvertIntegerUtil.parseIntOrDefault(row[0]))
+                    .date(date)
+                    .month(monthName)
+                    .year(year)
+                    .aid(StringUtil.processString(row[2]))
+                    .securityName(StringUtil.processString(row[3]))
+                    .faceValue(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[4]))
                     .marketPrice(row[5])
-                    .marketValue(parseBigDecimalOrDefault(row[6], null))
-                    .estimationSafekeepingFee(parseBigDecimalOrDefault(row[7], null))
+                    .marketValue(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[6]))
+                    .estimationSafekeepingFee(ConvertBigDecimalUtil.parseBigDecimalOrDefault(row[7]))
                     .build();
+
             sfValRgMonthlyList.add(sfValRgMonthly);
         }
 
         log.info("[Finish Map CSV] SfVal RG Monthly Size : {}", sfValRgMonthlyList.size());
         return sfValRgMonthlyList;
-    }
-
-    private static String processString(String value) {
-        // Check if the value is null and return a default value if needed
-        String processedValue = !value.isEmpty() ? value : "";
-
-        // Trim the string if it is not null
-        processedValue = processedValue.trim();
-
-        return processedValue;
-    }
-
-    private static String removeWhitespaceBetweenCharacters(String input) {
-        // Replace all whitespace between characters with an empty string
-        return input.replaceAll("\\s+", "").trim();
-    }
-
-    private static Integer parseIntOrDefault(String value) {
-        try {
-            return Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
-    private static LocalDate parseDateOrDefault(String value, DateTimeFormatter dateFormatter, LocalDate defaultValue) {
-        try {
-            return LocalDate.parse(value, dateFormatter);
-        } catch (Exception e) {
-            log.error("Parse Date is Failed : " + e.getMessage(), e);
-            return defaultValue;
-        }
-    }
-
-    private static BigDecimal parseBigDecimalOrDefault(String value, BigDecimal defaultValue) {
-        try {
-            return new BigDecimal(value);
-        } catch (Exception e) {
-            log.error("Parse BigDecimal is Failed : " + e.getMessage(), e);
-            return defaultValue;
-        }
     }
 
 }
