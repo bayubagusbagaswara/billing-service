0c4f6d448c2bdf2aacaa8c38fd8380151d959881
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 16:10:17 +0700

    add exception DataNotFoundException

---
+++ src/main/java/com/bayu/billingservice/exception/DataNotFoundException.java
@@ -0,0 +1,17 @@
+package com.bayu.billingservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DataNotFoundException extends RuntimeException {
+
+    public DataNotFoundException() {
+        super();
+    }
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
+}
