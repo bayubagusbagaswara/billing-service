baee470a837b32ca5e75befc71976960c5c76f05
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 16:19:03 +0700

    add service contract SfValRgDailyService

---
+++ src/main/java/com/bayu/billingservice/service/SfValRgDailyService.java
@@ -0,0 +1,24 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.SfValRgDaily;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SfValRgDailyService {
+
+    String readFileAndInsertToDB(String filePath);
+
+    List<SfValRgDaily> getAll();
+
+    List<SfValRgDaily> getAllByAid(String aid);
+
+    List<SfValRgDaily> getAllByAidAndDate(String aid, LocalDate date);
+
+    List<SfValRgDaily> getAllByAidAndMonthAndYear(String aid, String month, Integer year);
+
+    List<SfValRgDaily> getAllByAidAndSecurityName(String aid, String securityName);
+
+    String deleteAll();
+
+}
