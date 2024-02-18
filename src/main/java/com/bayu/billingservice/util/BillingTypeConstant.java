a3ff9b07213209b59fab7cc8daba0a0f81fc7deb
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 21:24:54 +0700

    add BillingTypeConstant

---
+++ src/main/java/com/bayu/billingservice/util/BillingTypeConstant.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BillingTypeConstant {
+
+    public static final String CORE_CATEGORY = "CORE";
+
+    public static final String CORE_TYPE_1 = "TYPE_1";
+    public static final String CORE_TYPE_2 = "TYPE_2";
+}
