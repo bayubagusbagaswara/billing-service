a2daa5f92fbaf55d6fc8c180e898b4af6ffb5fea
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 16:10:27 +0700

    implement logic

--- src/main/java/com/bayu/billingservice/service/impl/KycServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/KycServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bayu.billingservice.service.impl;
 
 import com.bayu.billingservice.dto.kyc.CreateKycRequest;
+import com.bayu.billingservice.exception.DataNotFoundException;
 import com.bayu.billingservice.model.Kyc;
 import com.bayu.billingservice.repository.KycRepository;
 import com.bayu.billingservice.service.KycService;
@@ -19,12 +20,21 @@ public class KycServiceImpl implements KycService {
 
     @Override
     public Kyc create(CreateKycRequest request) {
-        return null;
+        log.info("Create Kyc : {}", request);
+        Kyc kyc = Kyc.builder()
+                .aid(request.getAid())
+                .kseiSafeCode(request.getKseiSafeCode())
+                .billingCategory(request.getBillingCategory())
+                .billingType(request.getBillingType())
+                .build();
+
+        return kycRepository.save(kyc);
     }
 
     @Override
     public Kyc getByBillingCategoryAndBillingType(String billingCategory, String billingType) {
-        return null;
+        return kycRepository.findByBillingCategoryAndBillingType(billingCategory, billingType)
+                .orElseThrow(() -> new DataNotFoundException("Kyc not found with billing category : " + billingCategory + ", and billing type : " + billingType));
     }
 
 }
