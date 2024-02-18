0269b55a720b49d91463764bf85c77e077471355
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 20:21:54 +0700

    add util StringUtil

---
+++ src/main/java/com/bayu/billingservice/util/StringUtil.java
@@ -0,0 +1,24 @@
+package com.bayu.billingservice.util;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+public class StringUtil {
+
+    public static String processString(String value) {
+        // Check if the value is null and return a default value if needed
+        String processedValue = !value.isEmpty() ? value : "";
+
+        // Trim the string if it is not null
+        processedValue = processedValue.trim();
+
+        return processedValue;
+    }
+
+    public static String removeWhitespaceBetweenCharacters(String input) {
+        // Replace all whitespace between characters with an empty string
+        return input.replaceAll("\\s+", "").trim();
+    }
+}
