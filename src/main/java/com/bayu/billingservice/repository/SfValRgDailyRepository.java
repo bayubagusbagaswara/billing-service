844440ea5672e2c5dc3c4477de86ce5ac35e1152
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 20:30:42 +0700

    add some method contract

--- src/main/java/com/bayu/billingservice/repository/SfValRgDailyRepository.java
+++ src/main/java/com/bayu/billingservice/repository/SfValRgDailyRepository.java
@@ -2,19 +2,20 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.SfValRgDaily;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface SfValRgDailyRepository extends JpaRepository<SfValRgDaily, Long> {
 
-//    @Query(value = "SELECT * FROM sf_val_rg_daily WHERE aid = :aid", nativeQuery = true)
-//    List<SfValRgDaily> findAllByAid(@Param("aid") String aid);
+    List<SfValRgDaily> findAllByAid(String aid);
 
-//    List<SfValRgDaily> findAllByAidAndSecurityName();
+    List<SfValRgDaily> findAllByAidAndSecurityName(String aid, String securityName);
 
-//    List<SfValRgDaily> findAllByAidAndDate();
+    List<SfValRgDaily> findAllByAidAndDate(String aid, LocalDate date);
+
+    List<SfValRgDaily> findAllByAidAndYearAndMonth(String aid, Integer year, String monthName);
+
 }
