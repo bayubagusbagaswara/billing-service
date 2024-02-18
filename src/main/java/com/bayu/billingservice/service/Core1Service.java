9cbf6a82266536e18ca465b63bd1e9009b1f4ae3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 15:50:02 +0700

    add some method contract

--- src/main/java/com/bayu/billingservice/service/Core1Service.java
+++ src/main/java/com/bayu/billingservice/service/Core1Service.java
@@ -1,5 +1,15 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.dto.core.Core1DTO;
+
+import java.util.List;
+
 public interface Core1Service {
+
+    // response data
+    List<Core1DTO> calculate(String category, String type, String monthYear);
+
+    // Response message status
+    String calculate1();
 
 }
