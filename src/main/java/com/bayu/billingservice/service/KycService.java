a2ba4c37f045e325ccdbb328fe973e4941e59a09
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 16:05:19 +0700

    add some method contract

--- src/main/java/com/bayu/billingservice/service/KycService.java
+++ src/main/java/com/bayu/billingservice/service/KycService.java
@@ -1,8 +1,13 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.dto.kyc.CreateKycRequest;
+import com.bayu.billingservice.model.Kyc;
+
 public interface KycService {
 
     // create data Kyc
+    Kyc create(CreateKycRequest request);
 
     // get data Kyc by category billing and type billing
+    Kyc getByBillingCategoryAndBillingType(String billingCategory, String billingType);
 }
