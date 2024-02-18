ffa3322ef51dfc1316bdc2411436ecacc6ee8550
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-18 14:56:10 +0700

    add service contract SfValCoreIIGService

---
+++ src/main/java/com/bayu/billingservice/service/SfValCoreIIGService.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.SfValCoreIIG;
+
+import java.util.List;
+
+public interface SfValCoreIIGService {
+
+    // create
+    List<SfValCoreIIG> create();
+
+}
