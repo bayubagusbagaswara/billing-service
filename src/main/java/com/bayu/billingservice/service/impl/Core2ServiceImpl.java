c0b4f9a29f941f2f0437fb0695c96e4454f779d3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-20 21:35:09 +0700

    add service impl Core2ServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/Core2ServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.Core2Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class Core2ServiceImpl implements Core2Service {
+
+    @Override
+    public String calculate(String category, String type, String monthYear) {
+        return null;
+    }
+}
