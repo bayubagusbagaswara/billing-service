0449e7b564426c273e7775d7475e3983b1c5daa5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-24 17:09:47 +0700

    add model FeeParameter

---
+++ src/main/java/com/bayu/billingservice/model/FeeParameter.java
@@ -0,0 +1,32 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "fee_parameter", uniqueConstraints = {
+        @UniqueConstraint(name = "fee_parameter_name_unique", columnNames = "name")
+})
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeeParameter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "value")
+    private Double value;
+
+}
