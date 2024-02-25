04e9aeab8490d4d98d291659368ae0d3356f279a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 20:05:20 +0700

    add dto UserDTO

---
+++ src/main/java/com/bayu/billingservice/dto/user/UserDTO.java
@@ -0,0 +1,19 @@
+package com.bayu.billingservice.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+}
