8f57594206f93c7655a88e2082330d37803336d5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 11:22:59 +0700

    add method findLatestExchangeRateByName

--- src/main/java/com/bayu/billingservice/repository/ExchangeRateRepository.java
+++ src/main/java/com/bayu/billingservice/repository/ExchangeRateRepository.java
@@ -2,6 +2,7 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,8 @@ import java.util.Optional;
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
 
     Optional<ExchangeRate> findByCurrency(String currency);
+
+    @Query(value = "SELECT e FROM ExchangeRate e WHERE e.name = ?1 ORDER BY e.date DESC", nativeQuery = true)
+    Optional<ExchangeRate> findLatestExchangeRateByName(String name);
 
 }
