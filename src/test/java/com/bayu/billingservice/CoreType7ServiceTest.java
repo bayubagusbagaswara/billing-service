a6a8e8be01d5ac63e03da0d5e8c6f5b4533d4984
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 21:12:46 +0700

    add test CoreType7ServiceTest

---
+++ src/test/java/com/bayu/billingservice/CoreType7ServiceTest.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreType7ServiceTest {
+
+    // masukkan sample data 3 bulan terakhir
+
+
+    @Test
+    void name() {
+
+    }
+}
