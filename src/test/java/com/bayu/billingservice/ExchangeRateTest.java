22231af53cfcbe2a07434011bb6aee0d98fd9230
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 12:09:01 +0700

    update test

--- src/test/java/com/bayu/billingservice/ExchangeRateTest.java
+++ src/test/java/com/bayu/billingservice/ExchangeRateTest.java
@@ -1,7 +1,14 @@
 package com.bayu.billingservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ExchangeRateTest {
+
+    @Test
+    void name() {
+
+    }
+
 }
--- src/test/java/com/bayu/billingservice/KycCustomerTest.java
+++ src/test/java/com/bayu/billingservice/KycCustomerTest.java
@@ -1,9 +1,13 @@
 package com.bayu.billingservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class KycCustomerTest {
 
+    @Test
+    void name() {
 
+    }
 }
