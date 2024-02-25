1697effe523ba6dabf8c118e518370fda60d2209
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 11:05:46 +0700

    add model ExchangeRate

---
+++ src/main/java/com/bayu/billingservice/model/ExchangeRate.java
@@ -0,0 +1,29 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "exchange_rate")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "value")
+    private BigDecimal value;
+}
