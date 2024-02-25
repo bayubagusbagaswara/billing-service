f353c5d14f1c8ce354fa700b160a3775fbae5504
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 18:45:31 +0700

    update unit test ExchangeRateTest

--- src/test/java/com/bayu/billingservice/ExchangeRateTest.java
+++ src/test/java/com/bayu/billingservice/ExchangeRateTest.java
@@ -2,18 +2,15 @@ package com.bayu.billingservice;
 
 import com.bayu.billingservice.dto.exchangerate.CreateExchangeRateRequest;
 import com.bayu.billingservice.dto.exchangerate.ExchangeRateDTO;
+import com.bayu.billingservice.repository.ExchangeRateRepository;
 import com.bayu.billingservice.service.ExchangeRateService;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExchangeRateTest {
 
     private static final String CURRENCY_USD = "USD";
@@ -21,16 +18,25 @@ class ExchangeRateTest {
     @Autowired
     ExchangeRateService exchangeRateService;
 
-    @ParameterizedTest
-    @CsvSource({
-            "2024-02-23, 15590",
-            "2024-02-22, 15585",
-            "2024-02-21, 15630",
-            "2024-02-20, 15655",
-            "2024-02-19, 15625",
-            "2024-02-16, 15615",
-            "2024-02-15, 15615"
-    })
+    @Autowired
+    ExchangeRateRepository exchangeRateRepository;
+    @BeforeEach
+    void cleanUpAndInsertData() {
+        // Clean up existing data
+        exchangeRateRepository.deleteAll();
+    }
+
+    @Test
+    void insertData() {
+        createExchangeRate("2024-02-23", "15590");
+        createExchangeRate("2024-02-22", "15585");
+        createExchangeRate("2024-02-21", "15630");
+        createExchangeRate("2024-02-20", "15655");
+        createExchangeRate("2024-02-19", "15625");
+        createExchangeRate("2024-02-16", "15615");
+        createExchangeRate("2024-02-15", "15615");
+    }
+
     void createExchangeRate(String date, String value) {
         CreateExchangeRateRequest request = CreateExchangeRateRequest.builder()
                 .date(date)
