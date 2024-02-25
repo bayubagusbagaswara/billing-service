006e6fb28e808647a93b36c30c0f04239d0dca4a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 11:07:04 +0700

    add repository ExchangeRateRepository

---
+++ src/main/java/com/bayu/billingservice/repository/ExchangeRateRepository.java
@@ -0,0 +1,14 @@
+package com.bayu.billingservice.repository;
+
+import com.bayu.billingservice.model.ExchangeRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+
+    Optional<ExchangeRate> findByCurrency(String currency);
+
+}
