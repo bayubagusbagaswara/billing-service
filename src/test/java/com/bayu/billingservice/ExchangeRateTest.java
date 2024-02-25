4d9520c6a01ff5f606811dbdab8164515a6c9e4e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 17:11:12 +0700

    add unit test exchange rate

--- src/test/java/com/bayu/billingservice/ExchangeRateTest.java
+++ src/test/java/com/bayu/billingservice/ExchangeRateTest.java
@@ -1,14 +1,46 @@
 package com.bayu.billingservice;
 
-import org.junit.jupiter.api.Test;
+import com.bayu.billingservice.dto.exchangerate.CreateExchangeRateRequest;
+import com.bayu.billingservice.dto.exchangerate.ExchangeRateDTO;
+import com.bayu.billingservice.service.ExchangeRateService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-public class ExchangeRateTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ExchangeRateTest {
 
-    @Test
-    void name() {
+    private static final String CURRENCY_USD = "USD";
 
+    @Autowired
+    ExchangeRateService exchangeRateService;
+
+    @ParameterizedTest
+    @CsvSource({
+            "2024-02-23, 15590",
+            "2024-02-22, 15585",
+            "2024-02-21, 15630",
+            "2024-02-20, 15655",
+            "2024-02-19, 15625",
+            "2024-02-16, 15615",
+            "2024-02-15, 15615"
+    })
+    void createExchangeRate(String date, String value) {
+        CreateExchangeRateRequest request = CreateExchangeRateRequest.builder()
+                .date(date)
+                .currency(CURRENCY_USD)
+                .value(value)
+                .build();
+
+        ExchangeRateDTO exchangeRateDTO = exchangeRateService.create(request);
+        assertNotNull(exchangeRateDTO.getId());
+        assertEquals(exchangeRateDTO.getDate(), request.getDate());
     }
 
 }
