b0530df5020e7f78836701c045ed9636a105eba7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 21:09:36 +0700

    update UserService

--- src/main/java/com/bayu/billingservice/service/UserService.java
+++ src/main/java/com/bayu/billingservice/service/UserService.java
@@ -2,7 +2,6 @@ package com.bayu.billingservice.service;
 
 import com.bayu.billingservice.dto.user.UserDTO;
 import com.bayu.billingservice.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +9,11 @@ import java.util.List;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<UserDTO> fetchDataByCustomCondition(String customCondition) {
         return userRepository.findBySomeCustomCondition(customCondition);
