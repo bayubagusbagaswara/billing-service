753005e3c3d9358d6bbc4b0cf162f512d6d3e3dc
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 21:13:02 +0700

    add unit test SfValCoreIIGTest

--- src/test/java/com/bayu/billingservice/SfValCoreIIGTest.java
+++ src/test/java/com/bayu/billingservice/SfValCoreIIGTest.java
@@ -1,14 +1,33 @@
 package com.bayu.billingservice;
 
+import com.bayu.billingservice.service.SfValCoreIIGService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SfValCoreIIGTest {
+class SfValCoreIIGTest {
+
+    @Autowired
+    SfValCoreIIGService sfValCoreIIGService;
 
     @Test
-    void name() {
+    @DisplayName("Data Alama Manunggal")
+    void insertData1() {
+
 
     }
 
+    @Test
+    @DisplayName("Data Indo Infrastruktur")
+    void insertData2() {
+
+    }
+
+    @Test
+    @DisplayName("Data Mandala Kapital")
+    void insertData3() {
+
+    }
 }
