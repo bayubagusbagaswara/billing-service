b2c9a9fc6cacf3050d7d6a762dac0554b6d8c1b3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-25 12:50:35 +0700

    change return type method

--- src/main/java/com/bayu/billingservice/service/impl/KycCustomerServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/KycCustomerServiceImpl.java
@@ -1,12 +1,15 @@
 package com.bayu.billingservice.service.impl;
 
 import com.bayu.billingservice.dto.kyc.CreateKycRequest;
+import com.bayu.billingservice.dto.kyc.KycCustomerDTO;
 import com.bayu.billingservice.exception.DataNotFoundException;
 import com.bayu.billingservice.model.KycCustomer;
 import com.bayu.billingservice.repository.KycRepository;
 import com.bayu.billingservice.service.KycCustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -19,7 +22,7 @@ public class KycCustomerServiceImpl implements KycCustomerService {
     }
 
     @Override
-    public KycCustomer create(CreateKycRequest request) {
+    public KycCustomerDTO create(CreateKycRequest request) {
         log.info("Create Kyc : {}", request);
         KycCustomer kycCustomer = KycCustomer.builder()
                 .aid(request.getAid())
@@ -28,13 +31,33 @@ public class KycCustomerServiceImpl implements KycCustomerService {
                 .billingType(request.getBillingType())
                 .build();
 
-        return kycRepository.save(kycCustomer);
+        return mapToDTO(kycRepository.save(kycCustomer));
     }
 
     @Override
-    public KycCustomer getByBillingCategoryAndBillingType(String billingCategory, String billingType) {
-        return kycRepository.findByBillingCategoryAndBillingType(billingCategory, billingType)
+    public KycCustomerDTO getByBillingCategoryAndBillingType(String billingCategory, String billingType) {
+        KycCustomer kycCustomer = kycRepository.findByBillingCategoryAndBillingType(billingCategory, billingType)
                 .orElseThrow(() -> new DataNotFoundException("Kyc not found with billing category : " + billingCategory + ", and billing type : " + billingType));
+        return mapToDTO(kycCustomer);
+    }
+
+    private static KycCustomerDTO mapToDTO(KycCustomer kycCustomer) {
+        return KycCustomerDTO.builder()
+                .id(kycCustomer.getId())
+                .aid(kycCustomer.getAid())
+                .kseiSafeCode(kycCustomer.getKseiSafeCode())
+                .minimumFee(kycCustomer.getMinimumFee())
+                .customerSafekeepingFee(kycCustomer.getCustomerSafekeepingFee())
+                .journal(kycCustomer.getJournal())
+                .billingCategory(kycCustomer.getBillingCategory())
+                .billingType(kycCustomer.getBillingType())
+                .build();
+    }
+
+    private static List<KycCustomerDTO> mapToDTOList(List<KycCustomer> kycCustomerList) {
+        return kycCustomerList.stream()
+                .map(KycCustomerServiceImpl::mapToDTO)
+                .toList();
     }
 
 }
