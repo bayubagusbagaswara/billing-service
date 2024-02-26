be9b1bbc973012cf2b43a025337cb188c472c184
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-26 21:11:12 +0700

    add method contract

--- src/main/java/com/bayu/billingservice/service/impl/Core2ServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/Core2ServiceImpl.java
@@ -1,15 +1,24 @@
 package com.bayu.billingservice.service.impl;
 
+import com.bayu.billingservice.dto.core.Core2DTO;
 import com.bayu.billingservice.service.Core2Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class Core2ServiceImpl implements Core2Service {
 
+
     @Override
-    public String calculate(String category, String type, String monthYear) {
+    public List<Core2DTO> calculate(String category, String type, String monthYear) {
+        return null;
+    }
+
+    @Override
+    public String calculate1(String category, String type, String monthYear) {
         return null;
     }
 }
