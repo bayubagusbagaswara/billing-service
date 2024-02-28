be97c675c8a7fe1b6fd14e5dc53d7a498fa33b2c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-28 20:13:08 +0700

    delete

--- src/main/java/com/bayu/billingservice/repository/BillingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bayu.billingservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billing, Long> {
-}
