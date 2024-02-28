aefbd2e1584553758d3c56f6f4e4a255b6e04c5f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-02-28 21:49:30 +0700

    add controller Core4Controller

---
+++ src/main/java/com/bayu/billingservice/controller/Core4Controller.java
@@ -0,0 +1,13 @@
+package com.bayu.billingservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/core-4")
+@RequiredArgsConstructor
+public class Core4Controller {
+}
