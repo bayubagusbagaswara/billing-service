bc80b21f73aa40b6c562a25c8cd1515691cf8e8b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 16:01:41 +0700

    add method findFeeParameterByNameList

--- src/main/java/com/bayu/billingservice/repository/FeeParameterRepository.java
+++ src/main/java/com/bayu/billingservice/repository/FeeParameterRepository.java
@@ -2,8 +2,11 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.FeeParameter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,7 @@ public interface FeeParameterRepository extends JpaRepository<FeeParameter, Long
 
     Optional<FeeParameter> findByName(String name);
 
+    @Query(value = "SELECT f FROM MockFeeParameter f " +
+            "WHERE f.name IN :names", nativeQuery = true)
+    List<FeeParameter> findFeeParameterByNameList(@Param("names") List<String> names);
 }
