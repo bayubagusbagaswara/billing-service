31860b4d7061f5a47a74509e6c1dd6b2ccf0e0b4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 20:01:49 +0700

    add constant CurrencyConstant

---
+++ src/main/java/com/bayu/billingservice/constant/CurrencyConstant.java
@@ -0,0 +1,10 @@
+package com.bayu.billingservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CurrencyConstant {
+
+    public static final String CURRENCY_IDR = "IDR";
+    public static final String CURRENCY_USD = "USD";
+}
