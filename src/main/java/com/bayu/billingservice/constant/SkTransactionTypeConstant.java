cd44619260ca75b96e2a30fc2ed00cfa5980b913
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 21:55:57 +0700

    add constant SkTransactionTypeConstant

---
+++ src/main/java/com/bayu/billingservice/constant/SkTransactionTypeConstant.java
@@ -0,0 +1,11 @@
+package com.bayu.billingservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SkTransactionTypeConstant {
+
+    public static final String TRANSACTION_CBEST = "CBEST";
+    public static final String TRANSACTION_BI_SSSS = "BI-SSSS";
+
+}
