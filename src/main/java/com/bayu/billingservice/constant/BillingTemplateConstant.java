2f1447b8ba3dab5a7524b5fe9822a4073ececa74
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 20:04:39 +0700

    add constant BillingTemplateConstant

---
+++ src/main/java/com/bayu/billingservice/constant/BillingTemplateConstant.java
@@ -0,0 +1,17 @@
+package com.bayu.billingservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BillingTemplateConstant {
+
+    public static final String TEMPLATE_1 = "TEMPLATE_1";
+    public static final String TEMPLATE_2 = "TEMPLATE_2";
+    public static final String TEMPLATE_3 = "TEMPLATE_3";
+    public static final String TEMPLATE_4 = "TEMPLATE_4";
+    public static final String TEMPLATE_5 = "TEMPLATE_5";
+    public static final String TEMPLATE_6 = "TEMPLATE_6";
+    public static final String TEMPLATE_7 = "TEMPLATE_7";
+    public static final String TEMPLATE_8 = "TEMPLATE_8";
+
+}
