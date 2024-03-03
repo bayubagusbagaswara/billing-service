3957dde2e776628f863088ead87cfdba827889b2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 21:55:27 +0700

    update query

--- src/main/java/com/bayu/billingservice/repository/KseiSafekeepingFeeRepository.java
+++ src/main/java/com/bayu/billingservice/repository/KseiSafekeepingFeeRepository.java
@@ -13,15 +13,16 @@ import java.util.Optional;
 @Repository
 public interface KseiSafekeepingFeeRepository extends JpaRepository<KseiSafekeepingFee, Long> {
 
-    Optional<KseiSafekeepingFee> findByCustomerCodeContainingIgnoreCase(String feeAccount);
+    List<KseiSafekeepingFee> findByCustomerCodeContainingIgnoreCase(String feeAccount);
 
     @Query(value = "SELECT * FROM ksei_safekeeping_fee " +
             "WHERE customer_code = :customerCode " +
-            "AND date BETWEEN :startDate AND :endDate", nativeQuery = true)
+            "AND created_date BETWEEN :startDate AND :endDate", nativeQuery = true)
     List<KseiSafekeepingFee> findByCustomerCodeAndDateBetweenNative(
             @Param("customerCode") String customerCode,
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate);
+
     Optional<KseiSafekeepingFee> findByCustomerCodeAndMonthAndYear(String customerCode, String month, int year);
 
 }
