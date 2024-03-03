8778303ef325006c5a15ac80def72ccbe5ff5b1e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-03 11:34:00 +0700

    add service contract FundGeneratePDFServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/FundGeneratePDFServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.FundGeneratePDFService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FundGeneratePDFServiceImpl implements FundGeneratePDFService {
+
+    @Override
+    public String generatePDF() {
+        return null;
+    }
+}
