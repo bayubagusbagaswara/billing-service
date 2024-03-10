a54c88e9ef0fc7e5f9914c80f3e7007c1ae6a9ac
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 19:44:18 +0700

    add service impl Core6CalculateServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/Core6CalculateServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.Core6CalculateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Core6CalculateServiceImpl implements Core6CalculateService {
+}
