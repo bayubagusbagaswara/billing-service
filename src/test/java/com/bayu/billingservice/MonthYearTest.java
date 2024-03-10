828a3adddbe765359fd2a1410d4ac22c85cf2aee
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 20:41:45 +0700

    add unit test

--- src/test/java/com/bayu/billingservice/MonthYearTest.java
+++ src/test/java/com/bayu/billingservice/MonthYearTest.java
@@ -5,6 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 @Slf4j
 @SpringBootTest
 class MonthYearTest {
@@ -20,4 +26,27 @@ class MonthYearTest {
 
         log.info("Month 1 : {}, Year 1 : {}", month1, year1);
     }
+
+    @Test
+    void testConvertInstant() {
+        // Assuming you have an Instant object
+        Instant currentInstant = Instant.now();
+        Instant newInstant = currentInstant.plus(Duration.ofDays(14));
+
+        // Define a formatter with the desired pattern and locale
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yyyy")
+//                .withLocale(java.util.Locale.forLanguageTag("id-ID"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MMM-yy")
+                .withLocale(Locale.forLanguageTag("id-ID"));
+
+
+        // Set the time zone to Jakarta
+        ZoneId jakartaZone = ZoneId.of("Asia/Jakarta");
+
+        // Convert Instant to String
+        String formattedString = formatter.format(currentInstant.atZone(jakartaZone));
+
+        System.out.println("Formatted String: " + formattedString);
+    }
+
 }
