491a72ec12ba1359ea8a3d1d945f25e97c3368a5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 19:46:28 +0700

    add service impl Core8CalculateServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/Core8CalculateServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.Core8CalculateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Core8CalculateServiceImpl implements Core8CalculateService {
+}
