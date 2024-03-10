cd2f8e3781fadc9feda4ad10b29ad3d1d0b61e43
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 15:16:11 +0700

    add model BillingNumber

---
+++ src/main/java/com/bayu/billingservice/model/BillingNumber.java
@@ -0,0 +1,37 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "billing_account_number")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingNumber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sequence_number")
+    private int sequenceNumber;
+
+    @Column(name = "month")
+    private String month;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "number")
+    private String number;  // Column to store the original billing number
+}
