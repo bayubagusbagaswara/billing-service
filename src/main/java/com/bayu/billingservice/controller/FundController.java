2031da74ff5cf3fe07770199a5b0017c59546206
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 20:42:11 +0700

    update FundController

--- src/main/java/com/bayu/billingservice/controller/FundController.java
+++ src/main/java/com/bayu/billingservice/controller/FundController.java
@@ -4,7 +4,7 @@ import com.bayu.billingservice.dto.fund.BillingFundDTO;
 import com.bayu.billingservice.dto.ResponseDTO;
 import com.bayu.billingservice.dto.fund.FeeReportRequest;
 import com.bayu.billingservice.service.FundGeneratePDFService;
-import com.bayu.billingservice.service.FundService;
+import com.bayu.billingservice.service.FundCalculateService;
 import com.bayu.billingservice.util.ConvertDateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,11 +18,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FundController {
 
-    private final FundService fundService;
+    private final FundCalculateService fundCalculateService;
     private final FundGeneratePDFService fundGeneratePDFService;
 
     @PostMapping(path = "/calculate")
-    public ResponseEntity<ResponseDTO<List<BillingFundDTO>>> calculate(@RequestBody List<FeeReportRequest> reportRequests,
+    public ResponseEntity<ResponseDTO<String>> calculate(@RequestBody List<FeeReportRequest> reportRequests,
                                                                       @RequestParam("monthYear") String monthYear) {
 
         // format month year MMMM-yyyy
@@ -30,32 +30,26 @@ public class FundController {
         String month = monthFormat[0];
         int year = Integer.parseInt(monthFormat[1]);
 
-        List<BillingFundDTO> billingFundDTOList = fundService.calculate(reportRequests, month, year);
+        String status = fundCalculateService.calculate(reportRequests, month, year);
 
-        ResponseDTO<List<BillingFundDTO>> responseDTO = ResponseDTO.<List<BillingFundDTO>>builder()
+        ResponseDTO<String> responseDTO = ResponseDTO.<String>builder()
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.getReasonPhrase())
-                .payload(billingFundDTOList)
+                .payload(status)
                 .build();
 
         return ResponseEntity.ok().body(responseDTO);
     }
 
-    // get all billing fund
-
-    // update billing fund (request in List<>)
-
-
     @GetMapping(path = "/generate-pdf")
     public ResponseEntity<ResponseDTO<String>> generatePDF() {
-        // get all billing filter by status approval is Approved
         String statusGenerate = fundGeneratePDFService.generatePDF();
-
         ResponseDTO<String> response = ResponseDTO.<String>builder()
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.getReasonPhrase())
                 .payload(statusGenerate)
                 .build();
+
         return ResponseEntity.ok().body(response);
     }
 
