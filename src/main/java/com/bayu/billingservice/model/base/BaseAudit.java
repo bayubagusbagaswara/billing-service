4fda5bd8663f6e2c05da69b68cb70917ad029378
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 14:32:16 +0700

    add BaseAudit

---
+++ src/main/java/com/bayu/billingservice/model/base/BaseAudit.java
@@ -0,0 +1,30 @@
+package com.bayu.billingservice.model.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@Data
+@SuperBuilder
+@JsonIgnoreProperties(
+        value = { "createdAt", "updatedAt" },
+        allowGetters = true
+)
+public abstract class BaseAudit {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
