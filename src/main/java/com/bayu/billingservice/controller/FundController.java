468683d12b7158657906e6b4d971b98523488abb
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 15:18:12 +0700

    rename path api

--- src/main/java/com/bayu/billingservice/controller/FundController.java
+++ src/main/java/com/bayu/billingservice/controller/FundController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/billing-fund")
+@RequestMapping(path = "/api/fund")
 @RequiredArgsConstructor
 public class FundController {
 
