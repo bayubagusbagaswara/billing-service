9fefac73a2f396ea9da5805baad20219413b1672
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 21:32:15 +0700

    add method query

--- src/main/java/com/bayu/billingservice/repository/BillingFundRepository.java
+++ src/main/java/com/bayu/billingservice/repository/BillingFundRepository.java
@@ -2,8 +2,25 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.BillingFund;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BillingFundRepository extends JpaRepository<BillingFund, Long> {
+
+    @Query(value = "SELECT * FROM billing_funds " +
+            "WHERE billing_category = :billingCategory " +
+            "AND month = :month " +
+            "AND year = :year " +
+            "AND approval_status = :approvalStatus", nativeQuery = true)
+    List<BillingFund> findAllByBillingCategoryAndMonthAndYearAndApprovalStatus(
+            @Param("billingCategory") String billingCategory,
+            @Param("month") String month,
+            @Param("year") int year,
+            @Param("approvalStatus") String approvalStatus
+    );
+
 }
