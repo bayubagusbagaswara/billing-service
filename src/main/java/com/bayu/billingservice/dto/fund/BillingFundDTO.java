d361ee3e3abeb8823d406847361891cc720f1617
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 15:38:03 +0700

    add field billing number

--- src/main/java/com/bayu/billingservice/dto/fund/BillingFundDTO.java
+++ src/main/java/com/bayu/billingservice/dto/fund/BillingFundDTO.java
@@ -5,14 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BillingFundDTO {
+
+    private String billingNumber;
 
     private String portfolioCode;
 
