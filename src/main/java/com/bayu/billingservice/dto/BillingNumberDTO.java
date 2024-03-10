accf40a7cbdb2004312312c2edb98412392b44e1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 15:17:23 +0700

    add dto BillingNumberDTO

---
+++ src/main/java/com/bayu/billingservice/dto/BillingNumberDTO.java
@@ -0,0 +1,20 @@
+package com.bayu.billingservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingNumberDTO {
+
+    private String id;
+    private String sequenceNumber;
+    private String month;
+    private String year;
+    private String createdDate;
+    private String number;
+}
