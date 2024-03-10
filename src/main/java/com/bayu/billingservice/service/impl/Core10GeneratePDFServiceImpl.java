ff364b062ccee998e19dfd0e28a835063abc2c31
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 19:49:49 +0700

    add service impl Core10GeneratePDFServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/Core10GeneratePDFServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.Core10GeneratePDFService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Core10GeneratePDFServiceImpl implements Core10GeneratePDFService {
+}
