604106641538de8e6a28b3abd9705eb55f55aca1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 15:16:23 +0700

    add repository BillingNumberRepository

---
+++ src/main/java/com/bayu/billingservice/repository/BillingNumberRepository.java
@@ -0,0 +1,17 @@
+package com.bayu.billingservice.repository;
+
+import com.bayu.billingservice.model.BillingNumber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillingNumberRepository extends JpaRepository<BillingNumber, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT " +
+            "MAX(sequence_number) AS latest_sequence_number " +
+            "FROM billing_account_number " +
+            "WHERE month = :month AND year = :year")
+    Integer getMaxSequenceNumberByMonthAndYear(String month, int year);
+
+}
