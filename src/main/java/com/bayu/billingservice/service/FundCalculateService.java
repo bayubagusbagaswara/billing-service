e225dbcf67be3c7a7bc4543228d4334a82d1c0e0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-10 20:42:21 +0700

    update method return

---
+++ src/main/java/com/bayu/billingservice/service/FundCalculateService.java
@@ -0,0 +1,11 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.dto.fund.FeeReportRequest;
+
+import java.util.List;
+
+public interface FundCalculateService {
+
+    String calculate(List<FeeReportRequest> request, String month, int year);
+
+}
