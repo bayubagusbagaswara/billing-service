c74b13edd014a82b3f77d3a902ac0ed52b1fd4a4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-11 14:26:46 +0700

    add constant FundConstant

---
+++ src/main/java/com/bayu/billingservice/constant/FundConstant.java
@@ -0,0 +1,35 @@
+package com.bayu.billingservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FundConstant {
+
+    public static final String BILLING_NUMBER = "billingNumber";
+    public static final String BILLING_PERIOD = "billingPeriod";
+    public static final String BILLING_STATEMENT_DATE = "billingStatementDate";
+    public static final String BILLING_PAYMENT_DUE_DATE = "billingPaymentDueDate";
+    public static final String BILLING_CATEGORY = "billingCategory";
+    public static final String BILLING_TYPE = "billingType";
+    public static final String BILLING_TEMPLATE = "billingTemplate";
+
+    public static final String INVESTMENT_MANAGEMENT_NAME = "investmentManagementName";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS = "investmentManagementAddress";
+    public static final String PRODUCT_NAME = "productName";
+    public static final String ACCOUNT_NAME = "accountName";
+    public static final String ACCOUNT_NUMBER = "accountNumber";
+
+    public static final String CUSTOMER_FEE = "customerFee";
+    public static final String ACCRUAL_CUSTODIAL_FEE = "accrualCustodialFee";
+    public static final String BI_SSSS_VALUE_FREQUENCY = "bis4ValueFrequency";
+    public static final String BI_SSSS_TRANSACTION_FEE = "bis4TransactionFee";
+    public static final String BI_SSSS_AMOUNT_DUE = "bis4AmountDue";
+    public static final String SUB_TOTAL = "subTotal";
+    public static final String VAT_FEE = "vatFee";
+    public static final String VAT_AMOUNT_DUE = "vatAmountDue";
+    public static final String KSEI_VALUE_FREQUENCY = "kseiValueFrequency";
+    public static final String KSEI_TRANSACTION_FEE = "kseiTransactionFee";
+    public static final String KSEI_AMOUNT_DUE = "kseiAmountDue";
+    public static final String TOTAL_AMOUNT_DUE = "totalAmountDue";
+
+}
