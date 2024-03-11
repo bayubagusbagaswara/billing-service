5292f4b7e257f752a6acb40c1db11c497f052c67
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-11 14:27:08 +0700

    update method PdfGenerator

--- src/main/java/com/bayu/billingservice/util/PdfGenerator.java
+++ src/main/java/com/bayu/billingservice/util/PdfGenerator.java
@@ -3,18 +3,30 @@ package com.bayu.billingservice.util;
 import org.springframework.stereotype.Component;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Component
 public class PdfGenerator {
 
-    public void generatePdfFromHtml(String htmlContent, OutputStream outputStream) throws Exception {
-        try (OutputStream os = outputStream) {
+    public byte[] generatePdfFromHtml(String htmlContent) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             ITextRenderer renderer = new ITextRenderer();
             renderer.setDocumentFromString(htmlContent);
             renderer.layout();
-            renderer.createPDF(os, false);
+            renderer.createPDF(baos, false);
             renderer.finishPDF();
+            return baos.toByteArray();
         }
     }
+
+    public void savePdfToFile(byte[] pdfBytes, String outputPath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(new File(outputPath))) {
+            fos.write(pdfBytes);
+            fos.flush();
+        }
+    }
+
 }
