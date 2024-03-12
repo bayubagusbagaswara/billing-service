33487bbfb0af7e547290014d10304973e515b82a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 09:02:46 +0700

    add handler calculate

--- src/main/java/com/bayu/billingservice/controller/Core1Controller.java
+++ src/main/java/com/bayu/billingservice/controller/Core1Controller.java
@@ -1,11 +1,14 @@
 package com.bayu.billingservice.controller;
 
+import com.bayu.billingservice.dto.CoreCalculateRequest;
+import com.bayu.billingservice.dto.ResponseDTO;
 import com.bayu.billingservice.service.Core1CalculateService;
 import com.bayu.billingservice.service.Core1GeneratePDFService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -15,5 +18,18 @@ public class Core1Controller {
 
     private final Core1CalculateService calculateService;
     private final Core1GeneratePDFService generatePDFService;
+
+    @PostMapping(path = "/calculate")
+    public ResponseEntity<ResponseDTO<String>> calculate(@RequestBody CoreCalculateRequest request) {
+        String status = calculateService.calculate(request);
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .payload(status)
+                .build();
+
+        return ResponseEntity.ok().body(response);
+    }
 
 }
