f4319a33b486db2c8e4005b1d6c62fc852c7a2da
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 09:03:38 +0700

    update return data and parameter

--- src/main/java/com/bayu/billingservice/service/Core1CalculateService.java
+++ src/main/java/com/bayu/billingservice/service/Core1CalculateService.java
@@ -1,12 +1,10 @@
 package com.bayu.billingservice.service;
 
-import com.bayu.billingservice.dto.core.Core1DTO;
-
-import java.util.List;
+import com.bayu.billingservice.dto.CoreCalculateRequest;
 
 public interface Core1CalculateService {
 
-    List<Core1DTO> calculate(String category, String type, String monthYear);
+    String calculate(CoreCalculateRequest request);
 
 
 
