0b6f8e5b3bc7f98b024a6f6e2f208442612c6f51
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 14:28:41 +0700

    add unit test

--- src/test/java/com/bayu/billingservice/SfValCoreIIGTest.java
+++ src/test/java/com/bayu/billingservice/SfValCoreIIGTest.java
@@ -6,11 +6,31 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @SpringBootTest
 class SfValCoreIIGTest {
 
     @Autowired
     SfValCoreIIGService sfValCoreIIGService;
+
+
+    @Test
+    void testCalculateDivide100() {
+
+        // Input string
+        String inputString = "0.05";
+
+        // Convert the string to a BigDecimal
+        BigDecimal inputBigDecimal = new BigDecimal(inputString);
+
+        // Divide by 100
+        BigDecimal result = inputBigDecimal.divide(new BigDecimal(100), 4, RoundingMode.HALF_UP);
+
+        // Display the result
+        System.out.println("Result: " + result);
+    }
 
     @Test
     @DisplayName("Data Alama Manunggal")
