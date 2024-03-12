2ec0b55276dbba88589ea27ca052b63e9dc4e6b4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 08:33:36 +0700

    add fields

--- src/main/java/com/bayu/billingservice/dto/core/Core1DTO.java
+++ src/main/java/com/bayu/billingservice/dto/core/Core1DTO.java
@@ -1,36 +1,25 @@
 package com.bayu.billingservice.dto.core;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
-public class Core1DTO {
-
-    private String category;
-
-    private String type;
+public class Core1DTO extends BillingCoreBaseDTO {
 
     private String transactionHandlingValueFrequency;
-
     private String transactionHandlingFee;
-
     private String transactionHandlingAmountDue;
 
     private String safekeepingValueFrequency;
-
     private String safekeepingFee;
-
     private String safekeepingAmountDue;
 
-    private String totalAmountBeforeVAT;
+    private String subTotal;
 
     private String vatFee;
-
     private String vatAmountDue;
 
     private String totalAmountDue;
