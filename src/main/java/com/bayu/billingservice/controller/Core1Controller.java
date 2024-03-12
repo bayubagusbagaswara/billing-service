9a1665487a34438c891837737c8e9df3de160932
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 13:41:07 +0700

    add method handler

--- src/main/java/com/bayu/billingservice/controller/Core1Controller.java
+++ src/main/java/com/bayu/billingservice/controller/Core1Controller.java
@@ -2,6 +2,7 @@ package com.bayu.billingservice.controller;
 
 import com.bayu.billingservice.dto.CoreCalculateRequest;
 import com.bayu.billingservice.dto.ResponseDTO;
+import com.bayu.billingservice.dto.core.Core1DTO;
 import com.bayu.billingservice.service.Core1CalculateService;
 import com.bayu.billingservice.service.Core1GeneratePDFService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -27,6 +30,32 @@ public class Core1Controller {
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.getReasonPhrase())
                 .payload(status)
+                .build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping(path = "/generate-pdf")
+    public ResponseEntity<ResponseDTO<String>> generatePdf(@RequestBody CoreCalculateRequest request) {
+        String status = generatePDFService.generatePDF(request);
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .payload(status)
+                .build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<ResponseDTO<List<Core1DTO>>> getAll() {
+        List<Core1DTO> core1DTOList = generatePDFService.getAll();
+
+        ResponseDTO<List<Core1DTO>> response = ResponseDTO.<List<Core1DTO>>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .payload(core1DTOList)
                 .build();
 
         return ResponseEntity.ok().body(response);
