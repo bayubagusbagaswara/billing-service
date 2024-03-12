23be0964920c1cc44a50ee043c1ef4778d364026
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 14:28:12 +0700

    add method query

--- src/main/java/com/bayu/billingservice/repository/SfValCoreIIGRepository.java
+++ src/main/java/com/bayu/billingservice/repository/SfValCoreIIGRepository.java
@@ -2,6 +2,7 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.SfValCoreIIG;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface SfValCoreIIGRepository extends JpaRepository<SfValCoreIIG, Long
 
     List<SfValCoreIIG> findAllByCustomerCode(String customerCode);
 
+    @Query(value = "SELECT * FROM sf_val_core_iig WHERE customer_code = ?1 ORDER BY date ASC", nativeQuery = true)
+    List<SfValCoreIIG> findAllByCustomerCodeOrderByDateAsc(String customerCode);
 }
