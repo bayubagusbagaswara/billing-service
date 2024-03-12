0e35d4fe0f29cc9b35cc55f379f5884ff7f515be
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 16:17:42 +0700

    add method query

--- src/main/java/com/bayu/billingservice/repository/BillingFundRepository.java
+++ src/main/java/com/bayu/billingservice/repository/BillingFundRepository.java
@@ -1,5 +1,6 @@
 package com.bayu.billingservice.repository;
 
+import com.bayu.billingservice.model.BillingCore;
 import com.bayu.billingservice.model.BillingFund;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BillingFundRepository extends JpaRepository<BillingFund, Long> {
@@ -21,6 +23,20 @@ public interface BillingFundRepository extends JpaRepository<BillingFund, Long> 
             @Param("month") String month,
             @Param("year") int year,
             @Param("approvalStatus") String approvalStatus
+    );
+
+    @Query(value = "SELECT * FROM billing_funds " +
+            "WHERE aid = :aid " +
+            "AND bill_category = :billingCategory " +
+            "AND bill_type = :billingType " +
+            "AND month = :month " +
+            "AND year = :year", nativeQuery = true)
+    Optional<BillingFund> findByAidAndBillingCategoryAndBillingTypeAndMonthAndYear(
+            @Param("aid") String aid,
+            @Param("billingCategory") String billingCategory,
+            @Param("billingType") String billingType,
+            @Param("month") String monthName,
+            @Param("year") int year
     );
 
 }
