32d51036335dfc1dce7e4159f0cc50ab66338564
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 14:28:20 +0700

    add method contract

--- src/main/java/com/bayu/billingservice/service/SfValCoreIIGService.java
+++ src/main/java/com/bayu/billingservice/service/SfValCoreIIGService.java
@@ -1,12 +1,16 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.dto.iig.CreateSfValCoreIIGRequest;
 import com.bayu.billingservice.model.SfValCoreIIG;
 
 import java.util.List;
 
 public interface SfValCoreIIGService {
 
-    // create
-    List<SfValCoreIIG> create();
+    String create(CreateSfValCoreIIGRequest request);
+
+    List<SfValCoreIIG> getAll();
+
+    List<SfValCoreIIG> getAllByAidAndMonthYear(String aid, String monthYear);
 
 }
