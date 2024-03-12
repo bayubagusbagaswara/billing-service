c827ee3d5e7ac0adf9051781ec247caea1d146aa
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 09:03:20 +0700

    update enum

--- src/main/java/com/bayu/billingservice/model/enumerator/BillingType.java
+++ src/main/java/com/bayu/billingservice/model/enumerator/BillingType.java
@@ -2,17 +2,17 @@ package com.bayu.billingservice.model.enumerator;
 
 public enum BillingType {
 
-    TYPE_1("TYPE 1"),
-    TYPE_2("TYPE 2"),
-    TYPE_3("TYPE 3"),
-    TYPE_4("TYPE 4"),
-    TYPE_5("TYPE 5"),
-    TYPE_6("TYPE 6"),
-    TYPE_7("TYPE 7"),
-    TYPE_8("TYPE 8"),
-    TYPE_9("TYPE 9"),
-    TYPE_10("TYPE 10"),
-    TYPE_11("TYPE 11"),
+    TYPE_1("TYPE_1"),
+    TYPE_2("TYPE_2"),
+    TYPE_3("TYPE_3"),
+    TYPE_4("TYPE_4"),
+    TYPE_5("TYPE_5"),
+    TYPE_6("TYPE_6"),
+    TYPE_7("TYPE_7"),
+    TYPE_8("TYPE_8"),
+    TYPE_9("TYPE_9"),
+    TYPE_10("TYPE_10"),
+    TYPE_11("TYPE_11"),
     ;
     private final String value;
 
