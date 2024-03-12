44ade610414554cfb2f4aca82e1c57931fd3491b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 08:43:51 +0700

    add enum FeeParameter

---
+++ src/main/java/com/bayu/billingservice/model/enumerator/FeeParameter.java
@@ -0,0 +1,24 @@
+package com.bayu.billingservice.model.enumerator;
+
+public enum FeeParameter {
+
+    VAT("VAT"),
+    TRANSACTION_HANDLING_IDR("TRANSACTION_HANDLING_IDR"),
+    TRANSACTION_HANDLING_USD("TRANSACTION_HANDLING_USD"),
+    BI_SSSS("BI-SSSS"),
+    KSEI("KSEI"),
+    ADMINISTRATION_SET_UP("ADMINISTRATION_SET_UP"),
+    SIGNING_REPRESENTATION("SIGNING_REPRESENTATION"),
+    SECURITY_AGENT("SECURITY_AGENT"),
+    OTHER("OTHER");
+
+    private final String value;
+
+    FeeParameter(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
