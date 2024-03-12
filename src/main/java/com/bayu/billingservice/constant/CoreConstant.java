fc6fa05c5588e52bf5748572570bd569df580b67
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 13:40:52 +0700

    add constant

--- src/main/java/com/bayu/billingservice/constant/CoreConstant.java
+++ src/main/java/com/bayu/billingservice/constant/CoreConstant.java
@@ -5,4 +5,31 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CoreConstant {
 
+    public static final String BILLING_NUMBER = "billingNumber";
+    public static final String BILLING_PERIOD = "billingPeriod";
+    public static final String BILLING_STATEMENT_DATE = "billingStatementDate";
+    public static final String BILLING_PAYMENT_DUE_DATE = "billingPaymentDueDate";
+    public static final String BILLING_CATEGORY = "billingCategory";
+    public static final String BILLING_TYPE = "billingType";
+    public static final String BILLING_TEMPLATE = "billingTemplate";
+
+    public static final String INVESTMENT_MANAGEMENT_NAME = "investmentManagementName";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS = "investmentManagementAddress";
+    public static final String PRODUCT_NAME = "productName";
+    public static final String ACCOUNT_NAME = "accountName";
+    public static final String ACCOUNT_NUMBER = "accountNumber";
+    public static final String ACCOUNT_BANK = "accountBank";
+
+    public static final String TRANSACTION_HANDLING_VALUE_FREQUENCY = "transactionHandlingValueFrequency";
+    public static final String TRANSACTION_HANDLING_FEE = "transactionHandlingFee";
+    public static final String TRANSACTION_HANDLING_AMOUNT_DUE = "transactionHandlingAmountDue";
+
+    public static final String SAFEKEEPING_VALUE_FREQUENCY = "safekeepingValueFrequency";
+    public static final String SAFEKEEPING_FEE = "safekeepingFee";
+    public static final String SAFEKEEPING_AMOUNT_DUE = "safekeepingAmountDue";
+
+    public static final String SUB_TOTAL = "subTotal";
+    public static final String VAT_FEE = "vatFee";
+    public static final String VAT_AMOUNT_DUE = "vatAmountDue";
+    public static final String TOTAL_AMOUNT_DUE = "totalAmountDue";
 }
