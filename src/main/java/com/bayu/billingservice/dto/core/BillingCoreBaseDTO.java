9f7709131b70a95e07fb8ade2ff8e42599fc4d3d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 08:33:25 +0700

    add dto BillingCoreBaseDTO

---
+++ src/main/java/com/bayu/billingservice/dto/core/BillingCoreBaseDTO.java
@@ -0,0 +1,36 @@
+package com.bayu.billingservice.dto.core;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public abstract class BillingCoreBaseDTO {
+
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    private String approvalStatus;
+    private String portfolioCode;
+    private String month;
+    private String year;
+
+    private String billingNumber;
+    private String billingPeriod;
+    private String billingStatementDate;
+    private String billingPaymentDueDate;
+    private String billingCategory;
+    private String billingType;
+    private String billingTemplate;
+
+    private String investmentManagementName;
+    private String investmentManagementAddress;
+    private String productName;
+    private String accountName;
+    private String accountNumber;
+
+}
