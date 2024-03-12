626803f12bb4269caf4779a566a0b2549ac9c46c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 09:02:30 +0700

    add dto CoreCalculateRequest

---
+++ src/main/java/com/bayu/billingservice/dto/CoreCalculateRequest.java
@@ -0,0 +1,17 @@
+package com.bayu.billingservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoreCalculateRequest {
+
+    private String category; // format : Fund, Core, Retail
+
+    private String type; // format : Type 1, Type 2, Type 3, etc
+
+    private String monthYear; // format : Nov 2023, November 2023
+}
