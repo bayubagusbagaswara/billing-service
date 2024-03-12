4ce1ff61d67fba6a2553dab64dd1bf9fa52400b1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-12 13:41:15 +0700

    add method contract

--- src/main/java/com/bayu/billingservice/service/Core1GeneratePDFService.java
+++ src/main/java/com/bayu/billingservice/service/Core1GeneratePDFService.java
@@ -1,4 +1,13 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.dto.CoreCalculateRequest;
+import com.bayu.billingservice.dto.core.Core1DTO;
+
+import java.util.List;
+
 public interface Core1GeneratePDFService {
+
+    List<Core1DTO> getAll();
+
+    String generatePDF(CoreCalculateRequest request);
 }
