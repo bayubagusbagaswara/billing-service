3307088760a234b15a27e972e218e2a95ed018cf
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-03-17 13:38:27 +0700

    add some field

--- src/main/java/com/bayu/billingservice/model/BillingCustomer.java
+++ src/main/java/com/bayu/billingservice/model/BillingCustomer.java
@@ -20,39 +20,67 @@ public class BillingCustomer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "aid")
-    private String aid;
+    @Column(name = "customer_code")
+    private String customerCode; // aid
 
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_minimum_fee")
+    private BigDecimal customerMinimumFee; // 5.000.000 etc
+
+    @Column(name = "customer_safekeeping_fee")
+    private BigDecimal customerSafekeepingFee; // 0.5 etc
+
+    // MI information
     @Column(name = "mi_name")
     private String investmentManagementName;
 
     @Column(name = "mi_address")
     private String investmentManagementAddress;
 
-    @Column(name = "product_name")
-    private String productName;
-
+    // Account Transaction information
     @Column(name = "account_name")
     private String accountName;
 
     @Column(name = "account_number")
-    private String accountNumber;
+    private String accountNumber; //  GL 812017 tapi perlu dicek apakah accountNumber ini ada isinya atau tidak
+
+    @Column(name = "cost_center")
+    private String costCenter; // CC 943371
 
     @Column(name = "account_bank")
     private String accountBank;
 
+    @Column(name = "debit_transfer")
+    private BigDecimal debitTransfer;
+
+    @Column(name = "gl_account_result")
+    private String glAccountResult;
+
+    // NPWP information
+    @Column(name = "npwp_number")
+    private String npwpNumber;
+
+    @Column(name = "npwp_name")
+    private String npwpName;
+
+    @Column(name = "npwp_address")
+    private String npwpAddress;
+
+    // KSEI Safekeeping Fee information
     @Column(name = "ksei_safe_code")
     private String kseiSafeCode;
 
-    @Column(name = "minimum_fee")
-    private BigDecimal minimumFee; // 5.000.000 etc
+    // Selling Agent information
+    @Column(name = "selling_agent")
+    private String sellingAgent;
 
-    @Column(name = "customer_fee")
-    private BigDecimal customerFee; // 0.5 etc
+    // Other information
+    @Column(name = "currency")
+    private String currency;
 
-    @Column(name = "journal")
-    private String journal;
-
+    // Billing Information
     @Column(name = "billing_category")
     private String billingCategory;
 
