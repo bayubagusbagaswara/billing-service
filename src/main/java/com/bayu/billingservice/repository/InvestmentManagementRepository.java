e13d66ecf5101c5d8b19126382e9ee5f0eca6400
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 09:35:11 +0700

    add repository InvestmentManagementRepository

---
+++ src/main/java/com/bayu/billingservice/repository/InvestmentManagementRepository.java
@@ -0,0 +1,12 @@
+package com.bayu.billingservice.repository;
+
+import com.bayu.billingservice.model.InvestmentManagement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InvestmentManagementRepository extends JpaRepository<InvestmentManagement, Long> {
+
+    Optional<InvestmentManagement> findByCode(String code);
+
+}
