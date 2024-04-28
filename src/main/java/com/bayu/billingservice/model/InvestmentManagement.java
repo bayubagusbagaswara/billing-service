d27efa3a3830637ab25c753ace7e7d35d5bd1d95
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 09:33:57 +0700

    add entity InvestmentManagement

---
+++ src/main/java/com/bayu/billingservice/model/InvestmentManagement.java
@@ -0,0 +1,42 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "investment_managements")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvestmentManagement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address_1")
+    private String address1;
+
+    @Column(name = "address_2")
+    private String address2;
+
+    @Column(name = "address_3")
+    private String address3;
+
+    @Column(name = "address_4")
+    private String address4;
+
+}
