7d5911281863ffb404ac773c372bd08b784fa082
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 19:49:40 +0700

    add service impl BillingDataChangeServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/BillingDataChangeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.model.BillingDataChange;
+import com.bayu.billingservice.repository.BillingDataChangeRepository;
+import com.bayu.billingservice.service.BillingDataChangeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BillingDataChangeServiceImpl implements BillingDataChangeService {
+
+    private final BillingDataChangeRepository dataChangeRepository;
+
+    @Override
+    public List<BillingDataChange> getAll() {
+        return dataChangeRepository.findAll();
+    }
+}
