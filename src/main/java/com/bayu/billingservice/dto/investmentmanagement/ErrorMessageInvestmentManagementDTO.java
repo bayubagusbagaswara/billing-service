e0a9edaeb7343d1d9e778ec2e50178c669b9ad3e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 14:39:31 +0700

    add dto ErrorMessageInvestmentManagementDTO

---
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/ErrorMessageInvestmentManagementDTO.java
@@ -0,0 +1,20 @@
+package com.bayu.billingservice.dto.investmentmanagement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessageInvestmentManagementDTO {
+
+    private String code;
+
+    private List<String> errorMessageList;
+
+}
