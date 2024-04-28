22c88e96179b6ea8bb57c68e1c3bb399338552aa
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 14:38:53 +0700

    add json property

--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/CreateInvestmentManagementRequest.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/CreateInvestmentManagementRequest.java
@@ -1,5 +1,6 @@
 package com.bayu.billingservice.dto.investmentmanagement;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,17 +12,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateInvestmentManagementRequest {
 
+    @JsonProperty(value = "MI Code")
     private String code;
 
+    @JsonProperty(value = "MI Name")
     private String name;
 
+    @JsonProperty(value = "MI Email")
     private String email;
 
+    @JsonProperty(value = "Address 1")
     private String address1;
 
+    @JsonProperty(value = "Address 2")
     private String address2;
 
+    @JsonProperty(value = "Address 3")
     private String address3;
 
+    @JsonProperty(value = "Address 4")
     private String address4;
 }
