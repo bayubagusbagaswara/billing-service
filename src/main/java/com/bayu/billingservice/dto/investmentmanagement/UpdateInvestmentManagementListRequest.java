817309aa205ac0837c1b3a9a6904bcd2a95961c7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-29 05:32:21 +0700

    rename dto

--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/UpdateInvestmentManagementListRequest.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/UpdateInvestmentManagementListRequest.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UploadInvestmentManagementListRequest {
+public class UpdateInvestmentManagementListRequest {
 
     private Long dataChangeId;
 
