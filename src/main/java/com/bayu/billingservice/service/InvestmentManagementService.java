d02f81f0157fc3b5cb6a6f62f8653e69804eb574
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 10:50:59 +0700

    add service contract InvestmentManagementService

---
+++ src/main/java/com/bayu/billingservice/service/InvestmentManagementService.java
@@ -0,0 +1,21 @@
+package com.bayu.billingservice.service;
+
+public interface InvestmentManagementService {
+
+    // TRUE means the data is in the table
+    // check existing with code
+
+    // create single data
+
+    // create with upload data list
+
+    // get by code
+
+    // get all
+
+    // update by id
+
+    // update with upload data list
+
+    // delete by id
+}
