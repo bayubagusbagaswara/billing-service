7c769cb0bb09ef5cff1b931f4219846bc631a398
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-29 06:00:04 +0700

    update service method updateList

--- src/main/java/com/bayu/billingservice/service/impl/InvestmentManagementServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/InvestmentManagementServiceImpl.java
@@ -1,10 +1,8 @@
 package com.bayu.billingservice.service.impl;
 
-import com.bayu.billingservice.dto.investmentmanagement.CreateInvestmentManagementListRequest;
-import com.bayu.billingservice.dto.investmentmanagement.CreateInvestmentManagementListResponse;
-import com.bayu.billingservice.dto.investmentmanagement.ErrorMessageInvestmentManagementDTO;
-import com.bayu.billingservice.dto.investmentmanagement.InvestmentManagementDTO;
+import com.bayu.billingservice.dto.investmentmanagement.*;
 import com.bayu.billingservice.exception.CreateDataException;
+import com.bayu.billingservice.exception.DataNotFoundException;
 import com.bayu.billingservice.model.BillingDataChange;
 import com.bayu.billingservice.model.InvestmentManagement;
 import com.bayu.billingservice.model.enumerator.ActionStatus;
@@ -12,11 +10,13 @@ import com.bayu.billingservice.model.enumerator.ApprovalStatus;
 import com.bayu.billingservice.repository.BillingDataChangeRepository;
 import com.bayu.billingservice.repository.InvestmentManagementRepository;
 import com.bayu.billingservice.service.InvestmentManagementService;
+import com.bayu.billingservice.util.StringUtil;
 import com.bayu.billingservice.util.TableNameResolver;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
@@ -25,6 +25,7 @@ import org.springframework.validation.Validator;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -86,6 +87,62 @@ public class InvestmentManagementServiceImpl implements InvestmentManagementServ
     @Override
     public CreateInvestmentManagementListResponse createListApprove(CreateInvestmentManagementListRequest investmentManagementListRequest) {
         return null;
+    }
+
+    @Override
+    public UpdateInvestmentManagementListResponse updateList(UpdateInvestmentManagementListRequest investmentManagementListRequest) {
+        log.info("Request data: {}", investmentManagementListRequest);
+        Long dataChangeId = investmentManagementListRequest.getDataChangeId();
+        String inputId = investmentManagementListRequest.getInputId();
+        String inputIPAddress = investmentManagementListRequest.getInputIPAddress();
+        int totalDataSuccess = 0;
+        int totalDataFailed = 0;
+        List<ErrorMessageInvestmentManagementDTO> errorMessageList = new ArrayList<>();
+
+        try {
+            for (InvestmentManagementDTO dto : investmentManagementListRequest.getInvestmentManagementRequestList()) {
+                List<String> errorMessages = new ArrayList<>();
+                Errors errors = validateInvestmentManagementDTO(dto);
+
+                if (errors.hasErrors()) {
+                    errors.getAllErrors().forEach(error -> errorMessages.add(error.getDefaultMessage()));
+                }
+
+                if (isCodeAlreadyExists(dto.getCode())) {
+                    errorMessages.add("Code '" + dto.getCode() + "' is already taken");
+                }
+
+                if (errorMessages.isEmpty()) {
+                    InvestmentManagement investmentManagementEntity = investmentManagementRepository.findById(dto.getId())
+                            .orElseThrow(() -> new DataNotFoundException("Investment Management with id '" + dto.getId() + "' not found"));
+
+                    String jsonDataBefore = objectMapper.writeValueAsString(investmentManagementEntity);
+                    String jsonDataAfter = objectMapper.writeValueAsString(dto);
+                    BillingDataChange dataChangeEntity = dataChangeRepository.findById(dataChangeId)
+                            .orElseThrow(() -> new DataNotFoundException("Data Change with id '" + dataChangeId + "' not found"));
+                    dataChangeEntity.setActionStatus(ActionStatus.EDIT);
+                    dataChangeEntity.setInputId(inputId);
+                    dataChangeEntity.setInputIPAddress(inputIPAddress);
+                    dataChangeEntity.setInputDate(new Date());
+                    dataChangeEntity.setJsonDataBefore(jsonDataBefore);
+                    dataChangeEntity.setJsonDataAfter(jsonDataAfter);
+
+                    dataChangeRepository.save(dataChangeEntity);
+                    totalDataSuccess++;
+                } else {
+                    ErrorMessageInvestmentManagementDTO errorMessageDTO = new ErrorMessageInvestmentManagementDTO();
+                    errorMessageDTO.setCode(dto.getCode());
+                    errorMessageDTO.setErrorMessages(errorMessages);
+                    errorMessageList.add(errorMessageDTO);
+                    totalDataFailed++;
+                }
+            }
+
+            return new UpdateInvestmentManagementListResponse(totalDataSuccess, totalDataFailed, errorMessageList);
+        } catch (Exception e) {
+            log.error("Error when update list: {}", e.getMessage());
+            throw new CreateDataException("Error update investment management data", e);
+        }
     }
 
     private void saveToDataChange(InvestmentManagementDTO dto, String inputId, String inputIPAddress) {
