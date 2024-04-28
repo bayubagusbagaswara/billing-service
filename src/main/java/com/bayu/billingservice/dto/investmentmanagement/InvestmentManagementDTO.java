11be40450f5db88996670bd4b7c71f300b49c4b3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 14:39:02 +0700

    add dto InvestmentManagementDTO

---
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/InvestmentManagementDTO.java
@@ -0,0 +1,38 @@
+package com.bayu.billingservice.dto.investmentmanagement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvestmentManagementDTO {
+
+    private Long id;
+
+    @JsonProperty(value = "MI Code")
+    private String code;
+
+    @JsonProperty(value = "MI Name")
+    private String name;
+
+    @JsonProperty(value = "MI Email")
+    private String email;
+
+    @JsonProperty(value = "Address 1")
+    private String address1;
+
+    @JsonProperty(value = "Address 2")
+    private String address2;
+
+    @JsonProperty(value = "Address 3")
+    private String address3;
+
+    @JsonProperty(value = "Address 4")
+    private String address4;
+
+}
