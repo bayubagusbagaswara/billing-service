f47d5f9f40483fac6b4fd75e0989579f607b1f1a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 10:54:07 +0700

    add service impl InvestmentManagementServiceImpl

---
+++ src/main/java/com/bayu/billingservice/service/impl/InvestmentManagementServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.repository.InvestmentManagementRepository;
+import com.bayu.billingservice.service.InvestmentManagementService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InvestmentManagementServiceImpl implements InvestmentManagementService {
+
+    private final InvestmentManagementRepository investmentManagementRepository;
+}
