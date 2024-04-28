46cfad814aaa93d3c73a6dc22ff559e3d69d189e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 19:49:28 +0700

    add service contract BillingDataChangeService

---
+++ src/main/java/com/bayu/billingservice/service/BillingDataChangeService.java
@@ -0,0 +1,10 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.BillingDataChange;
+
+import java.util.List;
+
+public interface BillingDataChangeService {
+
+    List<BillingDataChange> getAll();
+}
