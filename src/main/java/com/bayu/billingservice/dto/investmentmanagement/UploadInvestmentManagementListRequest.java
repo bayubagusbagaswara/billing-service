3cbdd889e9ca15b96b344f94dac09e7f6d62555f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-04-28 14:39:55 +0700

    add dto UploadInvestmentManagementListRequest

---
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/UploadInvestmentManagementListRequest.java
@@ -0,0 +1,30 @@
+package com.bayu.billingservice.dto.investmentmanagement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadInvestmentManagementListRequest {
+
+    private Long dataChangeId;
+
+    private String approvalStatus;
+
+    private String inputId;
+
+    private String inputIPAddress;
+
+    private String approveId;
+
+    private String approveIPAddress;
+
+    private List<InvestmentManagementDTO> investmentManagementRequestList;
+
+}
