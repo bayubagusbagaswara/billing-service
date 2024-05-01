8245b315234f131adbb0ae602444a8d84b8908c6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-01 15:31:09 +0700

    add dto

---
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/DeleteInvestmentManagementListResponse.java
@@ -0,0 +1,21 @@
+package com.bayu.billingservice.dto.investmentmanagement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteInvestmentManagementListResponse {
+
+    private Integer totalDataSuccess;
+
+    private Integer totalDataFailed;
+
+    private List<ErrorMessageInvestmentManagementDTO> errorMessages;
+}
