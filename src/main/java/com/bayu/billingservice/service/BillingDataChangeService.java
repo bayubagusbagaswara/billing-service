0485ae627c80500ef312b205df774b1be2a5ef02
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-04 11:04:51 +0700

    add method contract createIsActionAdd

--- src/main/java/com/bayu/billingservice/service/BillingDataChangeService.java
+++ src/main/java/com/bayu/billingservice/service/BillingDataChangeService.java
@@ -1,5 +1,6 @@
 package com.bayu.billingservice.service;
 
+import com.bayu.billingservice.dto.datachange.BillingDataChangeDTO;
 import com.bayu.billingservice.model.BillingDataChange;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface BillingDataChangeService {
     List<BillingDataChange> getAll();
 
     String deleteAll();
+
+    <T> BillingDataChangeDTO createIsActionADD(BillingDataChangeDTO dataChangeDTO, Class<T> clazz);
+
 }
