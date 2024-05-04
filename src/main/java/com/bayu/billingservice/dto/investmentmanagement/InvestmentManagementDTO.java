7ce862aa76e036b4c68f4e4570d2cd78ff4604c8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-04 10:22:20 +0700

    remove unused annotation

--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/InvestmentManagementDTO.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/InvestmentManagementDTO.java
@@ -1,9 +1,6 @@
 package com.bayu.billingservice.dto.investmentmanagement;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -22,29 +19,22 @@ public class InvestmentManagementDTO {
 
     private Long id;
 
-//    @JsonProperty(value = "MI Code")
     @NotBlank(message = "Code cannot be empty")
     private String code;
 
-//    @JsonProperty(value = "MI Name")
     @NotBlank(message = "Name cannot be empty")
     private String name;
 
-//    @JsonProperty(value = "MI Email")
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email is not valid")
     private String email;
 
-//    @JsonProperty(value = "Address 1")
     private String address1;
 
-//    @JsonProperty(value = "Address 2")
     private String address2;
 
-//    @JsonProperty(value = "Address 3")
     private String address3;
 
-//    @JsonProperty(value = "Address 4")
     private String address4;
 
 }
