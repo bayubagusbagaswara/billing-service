667a54aa9abdefe3d7861f931ea309a659516645
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-05 14:34:39 +0700

    add repository SellingAgentRepository

---
+++ src/main/java/com/bayu/billingservice/repository/SellingAgentRepository.java
@@ -0,0 +1,7 @@
+package com.bayu.billingservice.repository;
+
+import com.bayu.billingservice.model.SellingAgent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellingAgentRepository extends JpaRepository<SellingAgent, Long> {
+}
