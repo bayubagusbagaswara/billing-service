6519361d871f97137b9333d804752d48961bfde0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-06 06:11:05 +0700

    add dto UpdateCustomerListResponse

---
+++ src/main/java/com/bayu/billingservice/dto/customer/UpdateCustomerListResponse.java
@@ -0,0 +1,20 @@
+package com.bayu.billingservice.dto.customer;
+
+import com.bayu.billingservice.dto.ErrorMessageDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCustomerListResponse {
+
+    private Integer totalDataSuccess;
+    private Integer totalDataFailed;
+    private List<ErrorMessageDTO> errorMessages;
+}
