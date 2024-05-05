670f9d2dfc35e950642fb29763db91eb5c77ee85
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-05 11:57:05 +0700

    remove unused annotation

--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/DeleteInvestmentManagementDTO.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/DeleteInvestmentManagementDTO.java
@@ -1,6 +1,5 @@
 package com.bayu.billingservice.dto.investmentmanagement;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
