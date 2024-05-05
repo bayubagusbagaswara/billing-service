b9173fc00012d88edef93e2349d3bbd49c310019
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-05 14:34:08 +0700

    add model selling agent

---
+++ src/main/java/com/bayu/billingservice/model/SellingAgent.java
@@ -0,0 +1,27 @@
+package com.bayu.billingservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "selling_agent")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SellingAgent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+}
