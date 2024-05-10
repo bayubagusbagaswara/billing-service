06be539c5409a02c642b9efef9486d6954ccb497
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-10 13:15:06 +0700

    add constructor

--- src/main/java/com/bayu/billingservice/exception/UnexpectedException.java
+++ src/main/java/com/bayu/billingservice/exception/UnexpectedException.java
@@ -9,4 +9,9 @@ public class UnexpectedException extends RuntimeException {
     public UnexpectedException(String message) {
         super(message);
     }
+
+    public UnexpectedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
