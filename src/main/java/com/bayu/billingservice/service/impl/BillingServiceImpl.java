c3d593963184614a606070c3e13101998a5042a2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-11 08:37:39 +0700

    add billing service

---
+++ src/main/java/com/bayu/billingservice/service/impl/BillingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bayu.billingservice.service.impl;
+
+import com.bayu.billingservice.service.BillingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceImpl implements BillingService {
+}
