52cdd555a5ee77e3a5beed1794463616a96ad696
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-11 09:30:42 +0700

    update some field customer dto

--- src/main/java/com/bayu/billingservice/dto/customer/CustomerDTO.java
+++ src/main/java/com/bayu/billingservice/dto/customer/CustomerDTO.java
@@ -2,13 +2,11 @@ package com.bayu.billingservice.dto.customer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.math.BigDecimal;
 
 @Data
 @Builder
@@ -21,35 +19,68 @@ public class CustomerDTO {
 
     private Long id;
 
+    // need a pattern (numeric & alphabet, not special character)
     @NotBlank(message = "Customer Code cannot be empty")
     private String customerCode;
 
-    @NotBlank(message = "Customer Name cannot be empty")
+    // need a pattern (numeric & alphabet, not special character)
+    @NotEmpty(message = "Customer Name cannot be empty")
     private String customerName;
 
-    @NotNull(message = "Customer Minimum Fee cannot be null")
-    private BigDecimal customerMinimumFee;
-
-    @NotNull(message = "Customer Safekeeping Fee cannot be null")
-    private BigDecimal customerSafekeepingFee;
-
-    private BigDecimal transactionHandling;
-
-    @NotBlank(message = "MI Code cannot be empty")
-    private String investmentManagementCode;
-    private String investmentManagementName;
-
-    private String accountName;
-    private String accountNumber;
-    private String costCenter;
-    private String accountBank;
-
-    private String kseiSafeCode;
-
+    @NotEmpty(message = "Billing Category cannot be empty")
     private String billingCategory;
+
+    @NotEmpty(message = "Billing Type cannot be empty")
     private String billingType;
+
+    @NotEmpty(message = "Billing Template cannot be empty")
     private String billingTemplate;
 
+    @NotEmpty(message = "Currency cannot be empty")
     private String currency;
+
+    @NotEmpty(message = "MI Code cannot be empty")
+    private String investmentManagementCode;
+
+    private String investmentManagementName;
+
+    // need a patter, must be numeric for string
+    private String account;
+
+    // need a patter, must be numeric for string
+    private String costCenterDebit;
+
+    // need a pattern (numeric & alphabet, not special character)
+    private String accountName;
+
+    // need a pattern, must be numeric for string
+    private String glAccountHasil;
+
+    // need a pattern must be numeric because big decimal
+    private String customerMinimumFee;
+
+    // need a pattern (must be numeric because big decimal)
+    @NotEmpty(message = "Customer Safekeeping Fee cannot be empty")
+    private String customerSafekeepingFee;
+
+    // need a pattern must be numeric because big decimal
+    private String transactionHandling;
+
+    // need a pattern, must be numeric for string
+    private String npwpNumber;
+
+    // need a pattern (numeric & alphabet, not special character)
+    private String npwpName;
+
+    private String npwpAddress;
+
+    // need a pattern, must be numeric for string
+    @NotEmpty(message = "Cost Center cannot be empty")
+    private String costCenter;
+
+    // need a pattern (numeric & alphabet, not special character)
+    private String kseiSafeCode;
+
+    // need a pattern (numeric & alphabet, not special character)
     private String sellingAgentCode;
 }
