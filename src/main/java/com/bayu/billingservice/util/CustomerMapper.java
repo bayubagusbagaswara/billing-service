dec1602bae983512e53e76bd73b9baf4f45e8e33
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-11 11:22:04 +0700

    add method mapFromCreateCustomerRequestToDTO

--- src/main/java/com/bayu/billingservice/util/CustomerMapper.java
+++ src/main/java/com/bayu/billingservice/util/CustomerMapper.java
@@ -1,11 +1,9 @@
 package com.bayu.billingservice.util;
 
+import com.bayu.billingservice.dto.customer.CreateCustomerRequest;
 import com.bayu.billingservice.dto.customer.CustomerDTO;
 import com.bayu.billingservice.model.Customer;
-import org.apache.xmlbeans.impl.xb.xsdschema.LocalSimpleType;
-import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.Mapping;
 
 import java.util.List;
 
@@ -16,26 +14,7 @@ public class CustomerMapper {
 
     public CustomerMapper(ModelMapperUtil modelMapperUtil) {
         this.modelMapperUtil = modelMapperUtil;
-//        modelMapper.addMappings(customerToDTOMapping);
     }
-
-//    private final PropertyMap<Customer, CustomerDTO> customerToDTOMapping = new PropertyMap<>() {
-//        @Override
-//        protected void configure() {
-//            // Menentukan pemetaan antara field-field Customer dan CustomerDTO
-//            map().setCustomerCode(source.getCustomerCode());
-//            map().setCustomerName(source.getCustomerName());
-//            map().setBillingCategory(source.getBillingCategory());
-//            // Menambahkan anotasi @Mapping untuk pemetaan khusus (jika diperlukan)
-//            map().setNpwpNumber(source.getNpwpNumber()); // Pemetaan langsung
-//            map().setNpwpName(source.getNpwpName()); // Pemetaan langsung
-//            map().setNpwpAddress(source.getNpwpAddress()); // Pemetaan langsung
-//            // Contoh penggunaan @Mapping dengan konversi kustom (String to BigDecimal)
-//            @Mapping(source = "customerMinimumFee", target = "customerMinimumFee", qualifiedByName = "stringToBigDecimal")
-//                    map().setCustomerMinimumFee(source.getCustomerMinimumFee());
-//        }
-//    };
-
 
     public Customer mapToEntity(CustomerDTO customerDTO) {
         Customer customer = new Customer();
@@ -53,6 +32,12 @@ public class CustomerMapper {
         return customerList.stream()
                 .map(this::mapToDTO)
                 .toList();
+    }
+
+    public CustomerDTO mapFromCreateCustomerRequestToDTO(CreateCustomerRequest createCustomerRequest) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        modelMapperUtil.mapObjects(createCustomerRequest, createCustomerRequest);
+        return customerDTO;
     }
 
 }
