782f171cd05f3860add8ce2ad2d07ef627cb7f57
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-11 11:52:53 +0700

    add method validate enum currency

--- src/main/java/com/bayu/billingservice/util/EnumValidator.java
+++ src/main/java/com/bayu/billingservice/util/EnumValidator.java
@@ -3,6 +3,7 @@ package com.bayu.billingservice.util;
 import com.bayu.billingservice.model.enumerator.BillingCategory;
 import com.bayu.billingservice.model.enumerator.BillingTemplate;
 import com.bayu.billingservice.model.enumerator.BillingType;
+import com.bayu.billingservice.model.enumerator.Currency;
 import lombok.experimental.UtilityClass;
 
 import java.util.Arrays;
@@ -13,7 +14,7 @@ public class EnumValidator {
     // Generic method to validate any enum type based on a string input
     private static <T extends Enum<T>> boolean validateEnum(Class<T> enumClass, String value) {
         return Arrays.stream(enumClass.getEnumConstants())
-                .anyMatch(enumConstant -> enumConstant.name().equalsIgnoreCase(value));
+                .noneMatch(enumConstant -> enumConstant.name().equalsIgnoreCase(value));
     }
 
     public static boolean validateEnumBillingCategory(String billingCategory) {
@@ -27,4 +28,9 @@ public class EnumValidator {
     public static boolean validateEnumBillingTemplate(String billingTemplate) {
         return validateEnum(BillingTemplate.class, billingTemplate);
     }
+
+    public static boolean validateEnumCurrency(String currency) {
+        return validateEnum(Currency.class, currency);
+    }
+
 }
