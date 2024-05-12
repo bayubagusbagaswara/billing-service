4725f0a7fc67d8c079a1ab6ab72719ed668ea6bd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-12 08:25:52 +0700

    update model SellingAgent

--- src/main/java/com/bayu/billingservice/model/SellingAgent.java
+++ src/main/java/com/bayu/billingservice/model/SellingAgent.java
@@ -1,18 +1,18 @@
 package com.bayu.billingservice.model;
 
+import com.bayu.billingservice.model.base.Approval;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "selling_agent")
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SellingAgent {
+public class SellingAgent extends Approval {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +24,24 @@ public class SellingAgent {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "gl")
+    private String gl;
+
+    @Column(name = "gl_name")
+    private String glName;
+
+    @Column(name = "account")
+    private String account;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "description")
+    private String description;
 }
