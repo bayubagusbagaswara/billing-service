ebe272f5932f8d83f5df4b637b467e7f13e46975
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-12 09:45:52 +0700

    add dto fee schedule

---
+++ src/main/java/com/bayu/billingservice/dto/feeschedule/DeleteFeeScheduleRequest.java
@@ -0,0 +1,19 @@
+package com.bayu.billingservice.dto.feeschedule;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteFeeScheduleRequest {
+
+    private String inputId;
+
+    private String inputIPAddress;
+
+    private Long id;
+}
