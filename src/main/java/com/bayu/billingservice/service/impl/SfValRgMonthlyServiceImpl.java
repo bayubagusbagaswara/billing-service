b7debe09d53e5aa97e82bde24f91d09f0f7be8b6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-15 05:41:37 +0700

    add handling error file path not found

--- src/main/java/com/bayu/billingservice/service/impl/SfValRgMonthlyServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/SfValRgMonthlyServiceImpl.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Slf4j
@@ -29,6 +31,12 @@ public class SfValRgMonthlyServiceImpl implements SfValRgMonthlyService {
     public String readFileAndInsertToDB(String filePath, String monthYear) {
         log.info("Start read and insert SfVal RG Monthly to the database : {}", filePath);
         try {
+            // Check if the file exists
+            if (!Files.exists(Paths.get(filePath))) {
+                log.error("File not found: {}", filePath);
+                throw new DataNotFoundException("[SfVal RG Monthly] File not found: " + filePath);
+            }
+
             String[] monthFormat = ConvertDateUtil.convertToYearMonthFormat(monthYear);
             String monthName = monthFormat[0];
             int year = Integer.parseInt(monthFormat[1]);
@@ -41,8 +49,15 @@ public class SfValRgMonthlyServiceImpl implements SfValRgMonthlyService {
             sfValRgMonthlyRepository.saveAll(sfValRgMonthlyList);
 
             return "[SfVal RG Monthly] CSV data processed and saved successfully";
-        } catch (IOException | CsvException e) {
-            return "[SfVal RG Monthly] Failed to process CSV File : " + e.getMessage();
+        }  catch (IOException e) {
+            log.error("Failed to read CSV file: {}", filePath, e);
+            return "[SfVal RG Monthly] Failed to read CSV File: " + e.getMessage();
+        } catch (CsvException e) {
+            log.error("Failed to process CSV data from file: {}", filePath, e);
+            return "[SfVal RG Monthly] Failed to process CSV data: " + e.getMessage();
+        } catch (Exception e) {
+            log.error("Unexpected error occurred while processing file: {}", filePath, e);
+            return "[SfVal RG Monthly] Unexpected error: " + e.getMessage();
         }
     }
 
