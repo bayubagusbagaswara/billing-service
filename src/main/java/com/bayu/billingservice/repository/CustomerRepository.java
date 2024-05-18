34d168082bdb9997af9263aab0e21783909a9ad9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-18 10:10:11 +0700

    add method query findByCustomerCodeAndOptionalSubCode

--- src/main/java/com/bayu/billingservice/repository/CustomerRepository.java
+++ src/main/java/com/bayu/billingservice/repository/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.bayu.billingservice.repository;
 import com.bayu.billingservice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +21,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query(value = "SELECT * FROM billing_customer WHERE billing_category = :category AND billing_type = :type", nativeQuery = true)
     List<Customer> findAllByBillingCategoryAndBillingType(String billingCategory, String billingType);
 
+    // Mengambil data dengan kondisi subCode bisa kosong (null)
+    @Query(value = "SELECT c FROM Customer c WHERE c.customerCode = :customerCode AND (:subCode IS NULL OR c.subCode = :subCode)", nativeQuery = true)
+    Optional<Customer> findByCustomerCodeAndOptionalSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
 }
