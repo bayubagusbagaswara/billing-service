f46bbafb2cc8d6b9d81f6d739f6c281097b91c43
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-18 17:29:11 +0700

    update dto

--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/CreateInvestmentManagementRequest.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/CreateInvestmentManagementRequest.java
@@ -1,6 +1,5 @@
 package com.bayu.billingservice.dto.investmentmanagement;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -18,28 +17,22 @@ public class CreateInvestmentManagementRequest {
 
     private String inputIPAddress;
 
-    @JsonProperty(value = "MI Code")
     @NotBlank(message = "Code cannot be empty")
     private String code;
 
-    @JsonProperty(value = "MI Name")
     @NotBlank(message = "Name cannot be empty")
     private String name;
 
-    @JsonProperty(value = "MI Email")
     @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email is not valid")
     private String email;
 
-    @JsonProperty(value = "Alamat 1")
+    @NotBlank(message = "Address 1 cannot be empty")
     private String address1;
 
-    @JsonProperty(value = "Alamat 2")
     private String address2;
 
-    @JsonProperty(value = "Alamat 3")
     private String address3;
 
-    @JsonProperty(value = "Alamat 4")
     private String address4;
 }
--- src/main/java/com/bayu/billingservice/dto/investmentmanagement/UpdateInvestmentManagementRequest.java
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/UpdateInvestmentManagementRequest.java
@@ -1,6 +1,5 @@
 package com.bayu.billingservice.dto.investmentmanagement;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -19,28 +18,22 @@ public class UpdateInvestmentManagementRequest {
 
     private Long id;
 
-    @JsonProperty(value = "MI Code")
-    @NotBlank(message = "Code cannot be blank")
+    @NotBlank(message = "Code cannot be empty")
     private String code;
 
-    @JsonProperty(value = "MI Name")
-    @NotBlank(message = "Name cannot be blank")
+    @NotBlank(message = "Name cannot be empty")
     private String name;
 
-    @JsonProperty(value = "MI Email")
-    @NotBlank(message = "Email cannot be blank")
+    @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email is not valid")
     private String email;
 
-    @JsonProperty(value = "Alamat 1")
+    @NotBlank(message = "Address 1 cannot be empty")
     private String address1;
 
-    @JsonProperty(value = "Alamat 2")
     private String address2;
 
-    @JsonProperty(value = "Alamat 3")
     private String address3;
 
-    @JsonProperty(value = "Alamat 4")
     private String address4;
 }
