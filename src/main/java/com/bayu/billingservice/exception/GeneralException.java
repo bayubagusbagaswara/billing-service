80ecc7dda5e130f752d8f84f42f0ca38cb66e027
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-18 17:27:27 +0700

    add exception GeneralException

---
+++ src/main/java/com/bayu/billingservice/exception/GeneralException.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice.exception;
+
+public class GeneralException extends RuntimeException {
+
+    public GeneralException() {
+        super();
+    }
+
+    public GeneralException(String message) {
+        super(message);
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
