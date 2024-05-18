dcd48f5a7d738fe0a32fd9f02bfa604630b1318b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-18 17:26:44 +0700

    add dto InvestmentManagementDataListRequest

---
+++ src/main/java/com/bayu/billingservice/dto/investmentmanagement/InvestmentManagementDataListRequest.java
@@ -0,0 +1,50 @@
+package com.bayu.billingservice.dto.investmentmanagement;
+
+import com.bayu.billingservice.dto.approval.ApprovalDTO;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class InvestmentManagementDataListRequest extends ApprovalDTO {
+
+    private Long id;
+
+    @JsonProperty(value = "MI Code")
+    @NotBlank(message = "Code cannot be empty")
+    private String code;
+
+    @JsonProperty(value = "MI Name")
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @JsonProperty(value = "MI Email")
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    @JsonProperty(value = "MI Unique Key")
+    @NotBlank(message = "Unique Key cannot be empty")
+    private String uniqueKey;
+
+    @JsonProperty(value = "Address 1")
+    @NotBlank(message = "Address 1 cannot be empty")
+    private String address1;
+
+    @JsonProperty(value = "Address 2")
+    private String address2;
+
+    @JsonProperty(value = "Address 3")
+    private String address3;
+
+    @JsonProperty(value = "Address 4")
+    private String address4;
+
+}
