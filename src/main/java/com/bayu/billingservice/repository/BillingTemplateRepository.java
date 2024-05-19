d48fac36e6e15d75396eadf8e427fc65d16323a7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-19 08:51:10 +0700

    update billing template service

--- src/main/java/com/bayu/billingservice/repository/BillingTemplateRepository.java
+++ src/main/java/com/bayu/billingservice/repository/BillingTemplateRepository.java
@@ -11,4 +11,7 @@ public interface BillingTemplateRepository extends JpaRepository<BillingTemplate
 
     @Query(value = "SELECT * FROM billing_template WHERE category = :category AND type = :type AND (:subCode IS NULL OR sub_code = :subCode)", nativeQuery = true)
     Optional<BillingTemplate> findByCategoryAndTypeAndSubCode(@Param("category") String category, @Param("type") String type, @Param("subCode") String subCode);
+
+    @Query(value = "SELECT COUNT(*) > 0 FROM billing_template WHERE category = :category AND type = :type AND (:subCode IS NULL OR sub_code = :subCode)", nativeQuery = true)
+    boolean existsByCategoryAndTypeAndSubCode(@Param("category") String category, @Param("type") String type, @Param("subCode") String subCode);
 }
--- src/main/java/com/bayu/billingservice/service/BillingTemplateService.java
+++ src/main/java/com/bayu/billingservice/service/BillingTemplateService.java
@@ -4,5 +4,7 @@ import com.bayu.billingservice.model.BillingTemplate;
 
 public interface BillingTemplateService {
 
+    boolean isExistsByCategoryAndTypeAndSubCode(String category, String type, String subCode);
+
     BillingTemplate getByCategoryAndTypeAndSubCode(String category, String type, String subCode);
 }
