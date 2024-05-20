565f62e741694e25fa2e51be3264044e29d5ac5f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-21 05:26:17 +0700

    add exception InvalidInputException

---
+++ src/main/java/com/bayu/billingservice/exception/InvalidInputException.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice.exception;
+
+public class InvalidInputException extends RuntimeException  {
+
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
