aa9d33e074905538c668816d652b6790c0f06f0f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-24 10:52:44 +0700

    add dto input and approval identifier

---
+++ src/main/java/com/bayu/billingservice/dto/ApprovalIdentifierRequest.java
@@ -0,0 +1,16 @@
+package com.bayu.billingservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApprovalIdentifierRequest {
+
+    private String approveIPAddress;
+
+}
