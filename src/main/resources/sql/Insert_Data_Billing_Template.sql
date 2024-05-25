dad40e1827e4caaa6f7f74605ec9c99807ed2b39
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-25 20:59:32 +0700

    add query

---
+++ src/main/resources/sql/Insert_Data_Billing_Template.sql
@@ -0,0 +1,5 @@
+INSERT INTO billing_template(template_name, category, type, sub_code, currency)
+VALUES ('FUND_TEMPLATE', 'FUND', 'TYPE_1', '', 'IDR'),
+    ('CORE_TEMPLATE_3', 'CORE', 'TYPE_1', '', 'IDR'),
+    ('CORE_TEMPLATE_5', 'CORE', 'TYPE_4', 'EB', 'IDR'),
+    ('CORE_TEMPLATE_3', 'CORE', 'TYPE_4', 'ITAMA', 'IDR');
---
+++ src/main/resources/sql/Insert_Data_Selling_Agent.sql
@@ -0,0 +1,4 @@
+INSERT INTO selling_agent(code, name)
+VALUES ('BDI', 'Bank Danamon Indonesia'),
+    ('INA', 'Bank Ina Perdana'),
+    ('CTBC', 'Bank CTBC Indonesia');
