465bf5e43f20f2e83bab76783293a2dd85f70c4e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-26 06:39:36 +0700

    remove unused class import

--- src/main/java/com/bayu/billingservice/repository/InvestmentManagementRepository.java
+++ src/main/java/com/bayu/billingservice/repository/InvestmentManagementRepository.java
@@ -2,9 +2,6 @@ package com.bayu.billingservice.repository;
 
 import com.bayu.billingservice.model.InvestmentManagement;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,9 +10,5 @@ public interface InvestmentManagementRepository extends JpaRepository<Investment
     Optional<InvestmentManagement> findByCode(String code);
 
     Boolean existsByCode(String code);
-
-//    @Modifying
-//    @Query(value = "DELETE FROM  WHERE month = :month AND year = :year", nativeQuery = true)
-//    void deleteByMonthAndYearNative(@Param("month") String month, @Param("year") Integer year);
 
 }
