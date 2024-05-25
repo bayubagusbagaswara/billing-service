4247e5ff6adedc6d4065c59a92e412ee8429ef95
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-25 15:15:28 +0700

    add method query existsCustomerByCustomerCodeAndSubCode

--- src/main/java/com/bayu/billingservice/repository/CustomerRepository.java
+++ src/main/java/com/bayu/billingservice/repository/CustomerRepository.java
@@ -14,14 +14,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Boolean existsByCustomerCode(String code);
 
-//    @Query(value = "select case when count(c)> 0 then true else false end from Customer c where lower(c.customerCode) = lower(:customerCode) AND lower(COALESCE(c.subCode,'')) = lower(COALESCE(:subCode, ''))")
-//    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END " +
-//        "FROM customer " +
-//        "WHERE LOWER(customer_code) = LOWER(:customerCode) " +
-//        "AND LOWER(COALESCE(sub_code, '')) = LOWER(COALESCE(:subCode, ''))",
-//        nativeQuery = true)
-//    Boolean existsCustomerByCustomerCodeAndSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
-//
     @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END FROM customer WHERE customer_code = :customerCode AND (:subCode IS NULL OR sub_code = :subCode)", nativeQuery = true)
     int countByCustomerCodeAndOptionalSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
 
@@ -32,13 +24,10 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query(value = "SELECT * FROM customer WHERE billing_category = :category AND billing_type = :type", nativeQuery = true)
     List<Customer> findAllByBillingCategoryAndBillingType(@Param("category") String billingCategory, @Param("type") String billingType);
 
-    // Mengambil data dengan kondisi subCode bisa kosong (null)
-//    @Query(value = "SELECT c FROM Customer c WHERE c.customerCode = :customerCode AND (:subCode IS NULL OR c.subCode = :subCode)")
-//    Optional<Customer> findByCustomerCodeAndOptionalSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
-
-    // Menggunakan native query untuk mengambil data dengan kondisi subCode bisa kosong (null)
     @Query(value = "SELECT * FROM customer WHERE customer_code = :customerCode AND (:subCode IS NULL OR sub_code = :subCode)", nativeQuery = true)
     Optional<Customer> findByCustomerCodeAndOptionalSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
 
+    @Query("SELECT COUNT(c) > 0 FROM Customer c WHERE LOWER(c.customerCode) = LOWER(:customerCode) AND LOWER(COALESCE(c.subCode, '')) = LOWER(COALESCE(:subCode, ''))")
+    boolean existsCustomerByCustomerCodeAndSubCode(@Param("customerCode") String customerCode, @Param("subCode") String subCode);
 
 }
