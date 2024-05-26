188ae7b83b1fd919e13a6cfdcf39c0e7b040540b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-26 10:36:02 +0700

    update query billing template

--- src/main/java/com/bayu/billingservice/repository/BillingTemplateRepository.java
+++ src/main/java/com/bayu/billingservice/repository/BillingTemplateRepository.java
@@ -15,12 +15,13 @@ public interface BillingTemplateRepository extends JpaRepository<BillingTemplate
 //    @Query(value = "SELECT EXISTS (SELECT 1 FROM billing_template WHERE category = :category AND type = :type AND (:subCode IS NULL OR sub_code = :subCode))", nativeQuery = true)
 //    boolean existsByCategoryAndTypeAndSubCode(@Param("category") String category, @Param("type") String type, @Param("subCode") String subCode);
 
-    @Query(value = "select case when count(c) > 0 then true else false end from BillingTemplate c " +
-            "WHERE lower(c.category) = lower(:category) " +
-            "AND lower(c.type) = lower(:type) " +
-            "AND lower(c.currency) = lower(:currency) " +
-            "AND lower(c.templateName) = lower(:templateName) " +
-            "AND lower(COALESCE(c.subCode,'')) = lower(COALESCE(:subCode, ''))")
+    // lower(COALESCE(c.subCode, '')) = lower(COALESCE(:subCode, ''))
+    @Query(value = "SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM BillingTemplate c " +
+            "WHERE LOWER(c.category) = LOWER(:category) " +
+            "AND LOWER(c.type) = LOWER(:type) " +
+            "AND LOWER(c.currency) = LOWER(:currency) " +
+            "AND LOWER(c.templateName) = LOWER(:templateName) " +
+            "AND (LOWER(c.subCode) = LOWER(:subCode) OR (c.subCode IS NULL AND :subCode IS NULL))")
     boolean existsByCategoryAndTypeAndCurrencyAndSubCodeAndTemplateName(
             @Param("category") String category,
             @Param("type") String type,
