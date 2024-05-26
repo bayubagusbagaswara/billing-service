0ddffa93352f236018aee1c2dc59140d6a80c91a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-05-26 11:54:42 +0700

    add util ClientIPUtil

---
+++ src/main/java/com/bayu/billingservice/util/ClientIPUtil.java
@@ -0,0 +1,23 @@
+package com.bayu.billingservice.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@UtilityClass
+@Slf4j
+public class ClientIPUtil {
+
+    public String getClientIp(HttpServletRequest request) {
+        String clientIp = request.getHeader("X-Forwarded-For");
+        if (clientIp == null || clientIp.isEmpty()) {
+            clientIp = request.getHeader("X-Real-IP");
+        }
+        if (clientIp == null || clientIp.isEmpty()) {
+            clientIp = request.getRemoteAddr();
+        }
+        log.info("Client IP Address: {}", clientIp);
+        return clientIp;
+    }
+
+}
