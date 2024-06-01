6da51fcd7d342fe391c34f8151349d7844e5fae3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-01 18:51:28 +0700

    add dto

---
+++ src/main/java/com/bayu/billingservice/dto/BillingCalculationResponse.java
@@ -0,0 +1,21 @@
+package com.bayu.billingservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingCalculationResponse {
+
+    private Integer totalDataSuccess;
+
+    private Integer totalDataFailed;
+
+    private List<BillingCalculationErrorMessageDTO> calculationErrorMessageList;
+}
