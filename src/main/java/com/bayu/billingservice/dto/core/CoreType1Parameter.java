3bfb96eb761f0da0cc415b1d810b5935634f268f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-02 19:55:16 +0700

    add dto core type 1 parameter

---
+++ src/main/java/com/bayu/billingservice/dto/core/CoreType1Parameter.java
@@ -0,0 +1,28 @@
+package com.bayu.billingservice.dto.core;
+
+import com.bayu.billingservice.model.SfValRgDaily;
+import com.bayu.billingservice.model.SkTransaction;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoreType1Parameter {
+
+    private String customerCode;
+
+    private List<SkTransaction> skTransactionList;
+
+    private BigDecimal transactionHandlingFee;
+
+    private List<SfValRgDaily> sfValRgDailyList;
+
+    private BigDecimal vatFee;
+}
