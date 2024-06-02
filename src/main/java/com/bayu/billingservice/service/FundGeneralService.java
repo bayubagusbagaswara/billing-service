360ae11e8e30ae17e42bb753085e80c94e45a3c7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-02 13:32:38 +0700

    add service contract FundGeneralService

---
+++ src/main/java/com/bayu/billingservice/service/FundGeneralService.java
@@ -0,0 +1,10 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.model.BillingFund;
+
+import java.util.List;
+
+public interface FundGeneralService {
+
+    List<BillingFund> getAll();
+}
