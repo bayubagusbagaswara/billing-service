4ebe7e47018e8e901405f74103c1de4932675bc7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-02 20:00:44 +0700

    add core template 1-8

--- src/main/java/com/bayu/billingservice/dto/core/CoreTemplate1.java
+++ src/main/java/com/bayu/billingservice/dto/core/CoreTemplate1.java
@@ -8,26 +8,16 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 /**
- * type 1, type 2, type 4 a (ITAMA), type 9, type 11
+ * type 5, type 6 (without NPWP)
  */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CoreTemplate3 {
+public class CoreTemplate1 {
 
     private Integer transactionHandlingValueFrequency;
     private BigDecimal transactionHandlingFee;
     private BigDecimal transactionHandlingAmountDue;
 
-    private BigDecimal safekeepingValueFrequency;
-    private BigDecimal safekeepingFee;
-    private BigDecimal safekeepingAmountDue;
-
-    private BigDecimal subTotal;
-
-    private BigDecimal vatFee;
-    private BigDecimal vatAmountDue;
-
-    private BigDecimal totalAmountDue;
 }
--- src/main/java/com/bayu/billingservice/dto/core/CoreTemplate2.java
+++ src/main/java/com/bayu/billingservice/dto/core/CoreTemplate2.java
@@ -8,26 +8,15 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 /**
- * type 1, type 2, type 4 a (ITAMA), type 9, type 11
+ * type 7.docx
  */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CoreTemplate3 {
+public class CoreTemplate2 {
 
     private Integer transactionHandlingValueFrequency;
     private BigDecimal transactionHandlingFee;
     private BigDecimal transactionHandlingAmountDue;
-
-    private BigDecimal safekeepingValueFrequency;
-    private BigDecimal safekeepingFee;
-    private BigDecimal safekeepingAmountDue;
-
-    private BigDecimal subTotal;
-
-    private BigDecimal vatFee;
-    private BigDecimal vatAmountDue;
-
-    private BigDecimal totalAmountDue;
 }
