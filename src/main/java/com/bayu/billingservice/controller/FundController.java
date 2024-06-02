91a30d033d1bdd23e74086717b717d27995f40e8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-02 14:25:10 +0700

    add method getAll

--- src/main/java/com/bayu/billingservice/controller/FundController.java
+++ src/main/java/com/bayu/billingservice/controller/FundController.java
@@ -1,11 +1,13 @@
 package com.bayu.billingservice.controller;
 
-import com.bayu.billingservice.dto.fund.BillingFundDTO;
+import com.bayu.billingservice.dto.BillingCalculationResponse;
 import com.bayu.billingservice.dto.ResponseDTO;
 import com.bayu.billingservice.dto.fund.FeeReportRequest;
+import com.bayu.billingservice.model.BillingFund;
+import com.bayu.billingservice.service.FundCalculateV2Service;
+import com.bayu.billingservice.service.FundGeneralService;
 import com.bayu.billingservice.service.FundGeneratePDFService;
 import com.bayu.billingservice.service.FundCalculateService;
-import com.bayu.billingservice.util.ConvertDateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,8 @@ public class FundController {
 
     private final FundCalculateService fundCalculateService;
     private final FundGeneratePDFService fundGeneratePDFService;
+    private final FundCalculateV2Service fundCalculateV2Service;
+    private final FundGeneralService fundGeneralService;
 
     @PostMapping(path = "/calculate")
     public ResponseEntity<ResponseDTO<String>> calculate(@RequestBody List<FeeReportRequest> reportRequests,
@@ -78,4 +82,28 @@ public class FundController {
         return ResponseEntity.ok().body(response);
     }
 
+    @PostMapping(path = "/calculate-v2")
+    public ResponseEntity<ResponseDTO<BillingCalculationResponse>> calculateV2(@RequestBody List<FeeReportRequest> reportRequests, @RequestParam("monthYear") String monthYear) {
+
+        BillingCalculationResponse calculate = fundCalculateV2Service.calculate(reportRequests, monthYear);
+
+        ResponseDTO<BillingCalculationResponse> responseDTO = ResponseDTO.<BillingCalculationResponse>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .payload(calculate)
+                .build();
+
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<ResponseDTO<List<BillingFund>>> getAll() {
+        List<BillingFund> all = fundGeneralService.getAll();
+        ResponseDTO<List<BillingFund>> response = ResponseDTO.<List<BillingFund>>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .payload(all)
+                .build();
+        return ResponseEntity.ok(response);
+    }
 }
