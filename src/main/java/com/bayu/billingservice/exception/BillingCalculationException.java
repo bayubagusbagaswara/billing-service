a53b5f01281e181f34ce900b548179dc9381e35e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-02 15:23:42 +0700

    override method

--- src/main/java/com/bayu/billingservice/exception/BillingCalculationException.java
+++ src/main/java/com/bayu/billingservice/exception/BillingCalculationException.java
@@ -1,9 +1,5 @@
 package com.bayu.billingservice.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class BillingCalculationException extends RuntimeException {
 
     public BillingCalculationException() {
@@ -13,4 +9,9 @@ public class BillingCalculationException extends RuntimeException {
     public BillingCalculationException(String message) {
         super(message);
     }
+
+    public BillingCalculationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
