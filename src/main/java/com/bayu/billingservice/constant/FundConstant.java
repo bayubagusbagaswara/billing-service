e1bc3ff19f33c01b7b67d187e94baad91d636bdf
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-03 20:01:08 +0700

    update values fund constant

--- src/main/java/com/bayu/billingservice/constant/FundConstant.java
+++ src/main/java/com/bayu/billingservice/constant/FundConstant.java
@@ -14,26 +14,33 @@ public class FundConstant {
     public static final String BILLING_TEMPLATE = "billingTemplate";
 
     public static final String INVESTMENT_MANAGEMENT_NAME = "investmentManagementName";
-    public static final String INVESTMENT_MANAGEMENT_ADDRESS_BUILDING = "investmentManagementAddressBuilding";
-    public static final  String INVESTMENT_MANAGEMENT_ADDRESS_STREET = "investmentManagementAddressStreet";
-    public static final String INVESTMENT_MANAGEMENT_ADDRESS_CITY = "investmentManagementAddressCity";
-    public static final  String INVESTMENT_MANAGEMENT_ADDRESS_PROVINCE = "investmentManagementAddressProvince";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS_1 = "investmentManagementAddress1";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS_2 = "investmentManagementAddress2";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS_3 = "investmentManagementAddress3";
+    public static final String INVESTMENT_MANAGEMENT_ADDRESS_4 = "investmentManagementAddress4";
+
     public static final String PRODUCT_NAME = "productName";
     public static final String ACCOUNT_NAME = "accountName";
     public static final String ACCOUNT_NUMBER = "accountNumber";
     public static final String ACCOUNT_BANK = "accountBank";
 
     public static final String CUSTOMER_FEE = "customerFee";
+
+    // TODO: tambahkan data accrual
     public static final String ACCRUAL_CUSTODIAL_FEE = "accrualCustodialFee";
-    public static final String BI_SSSS_VALUE_FREQUENCY = "bis4ValueFrequency";
+
+    public static final String BI_SSSS_TRANSACTION_VALUE_FREQUENCY = "bis4TransactionValueFrequency";
     public static final String BI_SSSS_TRANSACTION_FEE = "bis4TransactionFee";
-    public static final String BI_SSSS_AMOUNT_DUE = "bis4AmountDue";
+    public static final String BI_SSSS_TRANSACTION_AMOUNT_DUE = "bis4TransactionAmountDue";
+
     public static final String SUB_TOTAL = "subTotal";
+
     public static final String VAT_FEE = "vatFee";
     public static final String VAT_AMOUNT_DUE = "vatAmountDue";
-    public static final String KSEI_VALUE_FREQUENCY = "kseiValueFrequency";
+
+    public static final String KSEI_TRANSACTION_VALUE_FREQUENCY = "kseiTransactionValueFrequency";
     public static final String KSEI_TRANSACTION_FEE = "kseiTransactionFee";
-    public static final String KSEI_AMOUNT_DUE = "kseiAmountDue";
+    public static final String KSEI_TRANSACTION_AMOUNT_DUE = "kseiTransactionAmountDue";
     public static final String TOTAL_AMOUNT_DUE = "totalAmountDue";
 
 }
