0388e5b5c201cc39b7b51d2b457e610a766813a8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-04 21:21:07 +0700

    add service contract core type 5 and 6

---
+++ src/main/java/com/bayu/billingservice/service/CoreType5And6Service.java
@@ -0,0 +1,9 @@
+package com.bayu.billingservice.service;
+
+import com.bayu.billingservice.dto.core.CoreCalculateRequest;
+
+public interface CoreType5And6Service {
+
+    String calculate(CoreCalculateRequest request);
+
+}
