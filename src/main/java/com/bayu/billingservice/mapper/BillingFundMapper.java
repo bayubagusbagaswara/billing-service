ff08cf7237d0ddd4b7151fba415119eba6fc4605
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-09 10:59:13 +0700

    update mapper billing fund

--- src/main/java/com/bayu/billingservice/mapper/BillingFundMapper.java
+++ src/main/java/com/bayu/billingservice/mapper/BillingFundMapper.java
@@ -1,84 +1,65 @@
 package com.bayu.billingservice.mapper;
 
-import com.bayu.billingservice.dto.datachange.BillingDataChangeDTO;
 import com.bayu.billingservice.dto.fund.BillingFundDTO;
 import com.bayu.billingservice.model.BillingFund;
-import com.bayu.billingservice.model.enumerator.ApprovalStatus;
-import com.bayu.billingservice.util.ConvertDateUtil;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
+import com.bayu.billingservice.util.ConvertBigDecimalUtil;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class BillingFundMapper {
-    
-    private final ModelMapper modelMapper;
-    private final ConvertDateUtil convertDateUtil;
 
-    public BillingFundMapper(ModelMapper modelMapper, ConvertDateUtil convertDateUtil) {
-        this.modelMapper = modelMapper;
-        this.convertDateUtil = convertDateUtil;
-    }
-
-    private void configureMapper() {
-        modelMapper.getConfiguration().setSkipNullEnabled(true);
-
-        modelMapper.addMappings(new PropertyMap<BillingFund, BillingFundDTO>() {
-            @Override
-            protected void configure() {
-                skip(destination.getApprovalStatus());
-                skip(destination.getInputId());
-                skip(destination.getInputIPAddress());
-                skip(destination.getInputDate());
-                skip(destination.getApproveId());
-                skip(destination.getApproveIPAddress());
-                skip(destination.getApproveDate());
-            }
-        });
-    }
-
-    public BillingFund mapFromDtoToEntity(BillingFundDTO billingFundDTO) {
-        return modelMapper.map(billingFundDTO, BillingFund.class);
-    }
-
-    public BillingFundDTO mapFromEntityToDto(BillingFund billingFund) {
-        return modelMapper.map(billingFund, BillingFundDTO.class);
+    public BillingFundDTO mapToDTO(BillingFund billingFund) {
+        return BillingFundDTO.builder()
+                .id(billingFund.getId())
+                .createdAt(billingFund.getCreatedAt())
+                .updatedAt(billingFund.getUpdatedAt())
+                .billingStatus(billingFund.getBillingStatus().getStatus())
+                .approvalStatus(billingFund.getApprovalStatus().getStatus())
+                .customerCode(billingFund.getCustomerCode())
+                .subCode(billingFund.getSubCode())
+                .customerName(billingFund.getCustomerName())
+                .month(billingFund.getMonth())
+                .year(String.valueOf(billingFund.getYear()))
+                .billingNumber(billingFund.getBillingNumber())
+                .billingPeriod(billingFund.getBillingPeriod())
+                .billingStatementDate(billingFund.getBillingStatementDate())
+                .billingPaymentDueDate(billingFund.getBillingPaymentDueDate())
+                .billingCategory(billingFund.getBillingCategory())
+                .billingType(billingFund.getBillingType())
+                .billingTemplate(billingFund.getBillingTemplate())
+                .investmentManagementCode(billingFund.getInvestmentManagementCode())
+                .investmentManagementName(billingFund.getInvestmentManagementName())
+                .investmentManagementAddress1(billingFund.getInvestmentManagementAddress1())
+                .investmentManagementAddress2(billingFund.getInvestmentManagementAddress2())
+                .investmentManagementAddress3(billingFund.getInvestmentManagementAddress3())
+                .investmentManagementAddress4(billingFund.getInvestmentManagementAddress4())
+                .investmentManagementEmail(billingFund.getInvestmentManagementEmail())
+                .investmentManagementUniqueKey(billingFund.getInvestmentManagementUniqueKey())
+                .account(billingFund.getAccount())
+                .accountName(billingFund.getAccountName())
+                .currency(billingFund.getCurrency())
+                .accrualCustodialValueFrequency(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getAccrualCustodialValueFrequency()))
+                .accrualCustodialSafekeepingFee(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getAccrualCustodialSafekeepingFee()))
+                .accrualCustodialFee(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getAccrualCustodialFee()))
+                .bis4TransactionValueFrequency(String.valueOf(billingFund.getBis4TransactionValueFrequency()))
+                .bis4TransactionFee(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getBis4TransactionFee()))
+                .bis4TransactionAmountDue(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getBis4TransactionAmountDue()))
+                .subTotal(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getSubTotal()))
+                .vatFee(billingFund.getVatFee().stripTrailingZeros().toPlainString())
+                .vatAmountDue(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getVatAmountDue()))
+                .kseiTransactionValueFrequency(String.valueOf(billingFund.getKseiTransactionValueFrequency()))
+                .kseiTransactionFee(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getKseiTransactionFee()))
+                .kseiTransactionAmountDue(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getKseiTransactionAmountDue()))
+                .totalAmountDue(ConvertBigDecimalUtil.formattedBigDecimalToString(billingFund.getTotalAmountDue()))
+                .build();
     }
 
     public List<BillingFundDTO> mapToDTOList(List<BillingFund> billingFundList) {
         return billingFundList.stream()
-                .map(this::mapFromEntityToDto)
+                .map(this::mapToDTO)
                 .toList();
-    }
-
-    public BillingFund createEntity(BillingFundDTO billingFundDTO, BillingDataChangeDTO dataChangeDTO) {
-        BillingFund billingFund = modelMapper.map(billingFundDTO, BillingFund.class);
-        billingFund.setApprovalStatus(ApprovalStatus.APPROVED);
-        billingFund.setInputId(dataChangeDTO.getInputId());
-        billingFund.setInputIPAddress(dataChangeDTO.getInputIPAddress());
-        billingFund.setInputDate(dataChangeDTO.getInputDate());
-        billingFund.setApproveId(dataChangeDTO.getApproveId());
-        billingFund.setApproveIPAddress(dataChangeDTO.getApproveIPAddress());
-        billingFund.setApproveDate(convertDateUtil.getDate());
-        return billingFund;
-    }
-
-    public BillingFund updateEntity(BillingFund billingFundUpdated, BillingDataChangeDTO dataChangeDTO) {
-        BillingFund billingFund = modelMapper.map(billingFundUpdated, BillingFund.class);
-        billingFund.setApprovalStatus(ApprovalStatus.APPROVED);
-        billingFund.setInputId(dataChangeDTO.getInputId());
-        billingFund.setInputIPAddress(dataChangeDTO.getInputIPAddress());
-        billingFund.setInputDate(dataChangeDTO.getInputDate());
-        billingFund.setApproveId(dataChangeDTO.getApproveId());
-        billingFund.setApproveIPAddress(dataChangeDTO.getApproveIPAddress());
-        billingFund.setApproveDate(convertDateUtil.getDate());
-        return billingFund;
-    }
-
-    public void mapObjects(BillingFundDTO billingFundDTOSource, BillingFund billingFundTarget) {
-        modelMapper.map(billingFundDTOSource, billingFundTarget);
     }
 
 }
