08b9b690e32eab2fe733b1b9b6afa14c4152d1fa
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-09 16:32:52 +0700

    update service save

--- src/main/java/com/bayu/billingservice/service/impl/ReportGeneratorServiceImpl.java
+++ src/main/java/com/bayu/billingservice/service/impl/ReportGeneratorServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bayu.billingservice.service.impl;
 
+import com.bayu.billingservice.dto.reportgenerator.CreateReportGeneratorRequest;
 import com.bayu.billingservice.model.ReportGenerator;
 import com.bayu.billingservice.repository.ReportGeneratorRepository;
 import com.bayu.billingservice.service.ReportGeneratorService;
@@ -19,7 +20,26 @@ public class ReportGeneratorServiceImpl implements ReportGeneratorService {
     private final ConvertDateUtil convertDateUtil;
 
     @Override
-    public ReportGenerator save(ReportGenerator reportGenerator) {
+    public ReportGenerator save(CreateReportGeneratorRequest request) {
+        ReportGenerator reportGenerator = ReportGenerator.builder()
+                .createdAt(request.getDateNow())
+                .investmentManagementCode(request.getInvestmentManagementCode())
+                .investmentManagementName(request.getInvestmentManagementName())
+                .investmentManagementEmail(request.getInvestmentManagementEmail())
+                .investmentManagementUniqueKey(request.getInvestmentManagementUniqueKey())
+                .customerCode(request.getCustomerCode())
+                .customerName(request.getCustomerName())
+                .billingCategory(request.getBillingCategory())
+                .billingType(request.getBillingType())
+                .billingPeriod(request.getBillingPeriod())
+                .month(request.getMonth())
+                .year(request.getYear())
+                .currency(request.getCurrency())
+                .fileName(request.getFileName())
+                .filePath(request.getFilePath())
+                .status(request.getStatus())
+                .description(request.getDescription())
+                .build();
         return reportGeneratorRepository.save(reportGenerator);
     }
 
